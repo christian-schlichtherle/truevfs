dfd019b8831c4d46aa52ffd1556ed45b80127388
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2010-09-25 02:33:35 +0200

    Archive controller revisioned.

--- core/src/main/java/de/schlichtherle/truezip/io/archive/controller/ArchiveController.java
+++ core/src/main/java/de/schlichtherle/truezip/io/archive/controller/ArchiveController.java
@@ -15,6 +15,7 @@
  */
 package de.schlichtherle.truezip.io.archive.controller;
 
+import de.schlichtherle.truezip.io.socket.common.entry.CommonEntry;
 import de.schlichtherle.truezip.io.archive.filesystem.ArchiveFileSystemEntry;
 import de.schlichtherle.truezip.io.archive.ArchiveDescriptor;
 import de.schlichtherle.truezip.io.socket.common.input.CommonInputSocket;
@@ -23,11 +24,6 @@ import de.schlichtherle.truezip.util.BitField;
 import java.io.IOException;
 import java.net.URI;
 import javax.swing.Icon;
-
-import static de.schlichtherle.truezip.io.archive.entry.ArchiveEntry.SEPARATOR;
-import static de.schlichtherle.truezip.io.archive.entry.ArchiveEntry.SEPARATOR_CHAR;
-import static de.schlichtherle.truezip.io.archive.filesystem.ArchiveFileSystems.isRoot;
-import static de.schlichtherle.truezip.io.Paths.cutTrailingSeparators;
 
 /**
  * Provides multi-threaded read/write access to its <i>target archive file</i>
@@ -58,10 +54,7 @@ import static de.schlichtherle.truezip.io.Paths.cutTrailingSeparators;
  * @author Christian Schlichtherle
  * @version $Id$
  */
-public abstract class ArchiveController extends ArchiveDescriptor {
-
-    /** This class cannot get instantiated outside its package. */
-    ArchiveController() {}
+public interface ArchiveController extends ArchiveDescriptor {
 
     /**
      * {@inheritDoc}
@@ -71,14 +64,14 @@ public abstract class ArchiveController extends ArchiveDescriptor {
      * against this mount point.
      */
     @Override
-    public abstract URI getMountPoint();
+    URI getMountPoint();
 
     /**
      * Returns the controller for the enclosing archive file of this
      * controller's target archive file or {@code null} if it's not enclosed
      * in another archive file.
      */
-    public abstract ArchiveController getEnclController();
+    ArchiveController getEnclController();
 
     /**
      * Resolves the given relative {@code path} against the relative path of
@@ -87,35 +80,19 @@ public abstract class ArchiveController extends ArchiveDescriptor {
      * @throws NullPointerException if the target archive file is not enclosed
      *         within another archive file.
      */
-    public String getEnclPath(final String path) {
-        final URI enclPath = getEnclController()
-                .getMountPoint()
-                .relativize(getMountPoint());
-        final String result = isRoot(path)
-                ? cutTrailingSeparators(enclPath.toString(), SEPARATOR_CHAR)
-                : enclPath.resolve(path).toString();
-        assert result.endsWith(path);
-        assert !result.endsWith(SEPARATOR);
-        return result;
-    }
+    String getEnclPath(final String path);
 
-    public abstract Icon getOpenIcon()
-    throws FalsePositiveException;
+    Icon getOpenIcon() throws FalsePositiveException;
 
-    public abstract Icon getClosedIcon()
-    throws FalsePositiveException;
+    Icon getClosedIcon() throws FalsePositiveException;
 
-    public abstract boolean isReadOnly()
-    throws FalsePositiveException;
+    boolean isReadOnly() throws FalsePositiveException;
 
-    public abstract boolean isReadable(String path)
-    throws FalsePositiveException;
+    boolean isReadable(String path) throws FalsePositiveException;
 
-    public abstract boolean isWritable(String path)
-    throws FalsePositiveException;
+    boolean isWritable(String path) throws FalsePositiveException;
 
-    public abstract ArchiveFileSystemEntry getEntry(String path)
-    throws FalsePositiveException;
+    ArchiveFileSystemEntry getEntry(String path) throws FalsePositiveException;
 
     /**
      * Returns an archive input socket for reading the given entry from the
@@ -125,8 +102,7 @@ public abstract class ArchiveController extends ArchiveDescriptor {
      *         system.
      * @return A non-{@code null} {@code CommonInputSocket}.
      */
-    // TODO: Consider variant without options in order to implement InputSocketProvider<String> interface
-    public abstract CommonInputSocket<?>
+    CommonInputSocket<? extends CommonEntry>
     getInputSocket(BitField<ArchiveIOOption> options, String path)
     throws IOException;
 
@@ -138,24 +114,23 @@ public abstract class ArchiveController extends ArchiveDescriptor {
      *         system.
      * @return A non-{@code null} {@code CommonInputSocket}.
      */
-    // TODO: Consider variant without options in order to implement InputSocketProvider<String> interface
-    public abstract CommonOutputSocket<?>
+    CommonOutputSocket<? extends CommonEntry>
     getOutputSocket(BitField<ArchiveIOOption> options, String path)
     throws IOException;
 
-    public abstract boolean createNewFile(String path, boolean createParents)
+    boolean createNewFile(String path, boolean createParents)
     throws IOException;
 
-    public abstract boolean mkdir(String path, boolean createParents)
+    boolean mkdir(String path, boolean createParents)
     throws FalsePositiveException;
 
-    public abstract boolean delete(String path)
+    boolean delete(String path)
     throws FalsePositiveException;
 
-    public abstract boolean setLastModified(String path, long time)
+    boolean setLastModified(String path, long time)
     throws FalsePositiveException;
 
-    public abstract boolean setReadOnly(String path)
+    boolean setReadOnly(String path)
     throws FalsePositiveException;
 
     /**
@@ -173,6 +148,6 @@ public abstract class ArchiveController extends ArchiveDescriptor {
      * throughout the processing of the target archive file.
      * @see ArchiveControllers#sync(URI, BitField, ArchiveSyncExceptionBuilder)
      */
-    public abstract void sync(BitField<ArchiveSyncOption> options, ArchiveSyncExceptionBuilder builder)
+    void sync(BitField<ArchiveSyncOption> options, ArchiveSyncExceptionBuilder builder)
     throws ArchiveSyncException;
 }
