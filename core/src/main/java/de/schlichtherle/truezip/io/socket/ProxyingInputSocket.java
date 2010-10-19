39c0db0b83ced9886cd422e6f103b858c1933f65
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2010-10-19 14:19:03 +0200

    Revisioned.

--- core/src/main/java/de/schlichtherle/truezip/io/socket/ProxyingInputSocket.java
+++ core/src/main/java/de/schlichtherle/truezip/io/socket/ProxyingInputSocket.java
@@ -40,7 +40,15 @@ extends InputSocket<LT> {
         setInputSocket(input);
     }
 
-    protected final InputSocket<?> getInputSocket() {
+    /**
+     * Binds the proxied socket to this socket and returns it.
+     * If you override this method, you must make sure to bind the returned
+     * socket to this socket!
+     *
+     * @throws IOException at the discretion of an overriding method.
+     * @return The bound proxied socket.
+     */
+    protected InputSocket<?> getInputSocket() throws IOException {
         return input.bind(this);
     }
 
