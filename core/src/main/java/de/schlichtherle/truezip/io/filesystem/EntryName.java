5b40a7c17ebd82c49510eca0608a439c4c0c15d8
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2010-12-10 08:37:54 +0100

    Finished Path, MountPoint and EntryName classes.

---
+++ core/src/main/java/de/schlichtherle/truezip/io/filesystem/EntryName.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2010 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schlichtherle.truezip.io.filesystem;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static de.schlichtherle.truezip.io.filesystem.FileSystemEntry.SEPARATOR;
+
+/**
+ * Addresses an entry in a file system relative to its mount point.
+ * An entry name is usually constructed from a
+ * {@link URI Uniform Resource Identifier} in order to assert the following
+ * additional syntax constraints:
+ * <ol>
+ * <li>The URI must be relative, i.e. it must not have a scheme.
+ * <li>The URI must not have an authority.
+ * <li>The URI's path must be in normal form.
+ * <li>The URI's path must not equal {@code ".."}.
+ * <li>The URI's path must not start with {@code "/"}.
+ * <li>The URI's path must not start with {@code "./"}.
+ * <li>The URI's path must not start with {@code "../"}.
+ * </ol>
+ * <p>
+ * Examples for valid entry name URIs are:
+ * <ul>
+ * <li>{@code foo:bar:/baz!/bang} (mountPoint="bar:/baz", entryName="bang")
+ * <li>{@code foo:/bar/} (there are no constraints for hierarchical URIs.
+ * </ul>
+ * Examples for invalid path URIs are:
+ * <ul>
+ * <li>{@code foo:bar} (opaque URI w/o bang slash separator)
+ * <li>{@code foo:bar:baz:/bang!/} (dito)
+ * </ul>
+ * <p>
+ * Note that this class is immutable and final, hence thread-safe, too.
+ *
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+public final class EntryName implements Serializable, Comparable<EntryName> {
+
+    private final URI uri;
+
+    /**
+     * Equivalent to {@link #create(URI, boolean) create(uri, false)}.
+     */
+    public static EntryName create(URI uri) {
+        return create(uri, false);
+    }
+
+    /**
+     * Constructs a new entry name by parsing the given URI.
+     * This static factory method calls
+     * {@link #EntryName(URI, boolean) new EntryName(uri, normalize)}
+     * and wraps any thrown {@link URISyntaxException} in an
+     * {@link IllegalArgumentException}.
+     *
+     * @param  uri the non-{@code null} {@link #getUri() URI}.
+     * @param  normalize whether or not the given URI shall get normalized
+     *         before parsing it.
+     * @throws NullPointerException if {@code uri} is {@code null}.
+     * @throws URISyntaxException if {@code uri} does not conform to the
+     *         syntax constraints for entry names.
+     * @return A non-{@code null} entry name.
+     */
+    public static EntryName create(URI uri, boolean normalize) {
+        try {
+            return new EntryName(uri, normalize);
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    /**
+     * Equivalent to {@link #EntryName(URI, boolean) new EntryName(uri, false)}.
+     */
+    public EntryName(URI uri) throws URISyntaxException {
+        this(uri, false);
+    }
+
+    /**
+     * Constructs a new entry name by parsing the given URI.
+     *
+     * @param  uri the non-{@code null} {@link #getUri() URI}.
+     * @param  normalize whether or not the given URI shall get normalized
+     *         before parsing it.
+     * @throws NullPointerException if {@code uri} is {@code null}.
+     * @throws URISyntaxException if {@code uri} does not conform to the
+     *         syntax constraints for entry names.
+     */
+    public EntryName(URI uri, final boolean normalize)
+    throws URISyntaxException {
+        if (uri.isAbsolute())
+            throw new URISyntaxException(uri.toString(),
+                    "Scheme not allowed");
+        if (uri.getRawAuthority() != null)
+            throw new URISyntaxException(uri.toString(),
+                    "Authority not allowed");
+        if (normalize) {
+            uri = uri.normalize();
+            String p = uri.getPath();
+            while (true)
+                if (p.equals(".."))
+                    p = p.substring(2);
+                else if (p.startsWith(SEPARATOR))
+                    p = p.substring(1);
+                else if (p.startsWith("." + SEPARATOR))
+                    p = p.substring(3);
+                else if (p.startsWith(".." + SEPARATOR))
+                    p = p.substring(3);
+                else
+                    break;
+            uri = new URI(null, null, p, uri.getQuery(), uri.getFragment());
+        } else {
+            if (uri.normalize() != uri)
+                throw new URISyntaxException(uri.toString(),
+                        "Path not in normal form");
+            final String p = uri.getRawPath();
+            if (    "..".equals(p) || p.startsWith(SEPARATOR)
+                    || p.startsWith("." + SEPARATOR)
+                    || p.startsWith(".." + SEPARATOR))
+                throw new URISyntaxException(uri.toString(),
+                        "Illegal start of path");
+        }
+        this.uri = uri;
+
+        assert invariants();
+    }
+
+    private boolean invariants() {
+        assert null != uri;
+        assert !uri.isAbsolute();
+        assert null == uri.getRawAuthority();
+        assert uri.normalize() == uri;
+        String p = uri.getRawPath();
+        assert !"..".equals(p);
+        assert !p.startsWith(SEPARATOR);
+        assert !p.startsWith(".." + SEPARATOR);
+        return true;
+    }
+
+    /**
+     * Returns the non-{@code null} URI of this entry name.
+     *
+     * @return The non-{@code null} URI of this entry name.
+     */
+    public URI getUri() {
+        return uri;
+    }
+
+    /**
+     * Returns {@code true} iff the given object is an entry name and its URI
+     * {@link URI#equals(Object) equals} the URI of this entry name.
+     */
+    @Override
+    public boolean equals(final Object that) {
+        return this == that
+                || that instanceof EntryName
+                    && this.uri.equals(((EntryName) that).uri);
+    }
+
+    /**
+     * Implements a natural ordering which is consistent with
+     * {@link #equals(Object)}.
+     */
+    @Override
+    public int compareTo(final EntryName that) {
+        return this.uri.compareTo(that.uri);
+    }
+
+    /**
+     * Returns a hash code which is consistent with {@link #equals(Object)}.
+     */
+    @Override
+    public int hashCode() {
+        return uri.hashCode();
+    }
+
+    /**
+     * Equivalent to calling {@link URI#toString()} on {@link #getUri()}.
+     */
+    @Override
+    public String toString() {
+        return uri.toString();
+    }
+}
---
+++ core/src/main/java/de/schlichtherle/truezip/io/filesystem/MountPoint.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2010 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schlichtherle.truezip.io.filesystem;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static de.schlichtherle.truezip.io.filesystem.Path.BANG_SLASH;
+
+/**
+ * Addresses the mount point of a file system.
+ * A mount point is usually constructed from a
+ * {@link URI Uniform Resource Identifier} in order to assert the following
+ * additional syntax constraints:
+ * <ol>
+ * <li>The URI must be absolute.
+ * <li>The URI must not have a fragment.
+ * <li>If the URI is opaque, its scheme specific part must end with the bang
+ *     slash separator {@code "!/"}.
+ *     The scheme specific part <em>before</em> this bang slash separator is
+ *     parsed according the syntax constraints for the {@link #getPath() path}
+ *     with the following additional syntax constraints:
+ *     The path must be absolute and cannot have a fragment.
+ *     If its opaque, it's entry name must not be empty.
+ *     If its hierarchical, it's path must be in normal form.
+ * <li>If the URI is hierarchical, its path must be in normal form.
+ * </ol>
+ * <p>
+ * Examples for valid mount point URIs are:
+ * <ul>
+ * <li>{@code foo:/bar}
+ * <li>{@code foo:bar:/baz!/}
+ * <li>{@code foo:bar:baz:/bang!/boom!/}
+ * </ul>
+ * Examples for invalid mount point URIs are:
+ * <ul>
+ * <li>{@code foo} (not absolute)
+ * <li>{@code foo:/bar#baz} (fragment)
+ * <li>{@code foo:bar:/baz!/bang} (doesn't end with bang slash separator)
+ * </ul>
+ * <p>
+ * Note that this class is immutable and final, hence thread-safe, too.
+ *
+ * @see     Path
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+public final class MountPoint implements Serializable, Comparable<MountPoint> {
+
+    private static final long serialVersionUID = 5723957985634276648L;
+
+    private final URI uri;
+    private final Path path;
+
+    /**
+     * Equivalent to {@link #create(URI, boolean) create(uri, false)}.
+     */
+    public static MountPoint create(URI uri) {
+        return create(uri, false);
+    }
+
+    /**
+     * Constructs a new mount point by parsing the given URI.
+     * This static factory method calls
+     * {@link #MountPoint(URI, boolean) new MountPoint(uri, normalize)}
+     * and wraps any thrown {@link URISyntaxException} in an
+     * {@link IllegalArgumentException}.
+     *
+     * @param  uri the non-{@code null} {@link #getUri() URI}.
+     * @param  normalize whether or not the given URI shall get normalized
+     *         before parsing it.
+     * @throws NullPointerException if {@code uri} is {@code null}.
+     * @throws URISyntaxException if {@code uri} does not conform to the
+     *         syntax constraints for mount points.
+     * @return A non-{@code null} mount point.
+     */
+    public static MountPoint create(URI uri, boolean normalize) {
+        try {
+            return new MountPoint(uri, normalize);
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    /**
+     * Equivalent to {@link #MountPoint(URI, boolean) new MountPoint(uri, false)}.
+     */
+    public MountPoint(URI uri) throws URISyntaxException {
+        this(uri, false);
+    }
+
+    /**
+     * Constructs a new mount point by parsing the given URI.
+     *
+     * @param  uri the non-{@code null} {@link #getUri() URI}.
+     * @param  normalize whether or not the given URI shall get normalized
+     *         before parsing it.
+     * @throws NullPointerException if {@code uri} is {@code null}.
+     * @throws URISyntaxException if {@code uri} does not conform to the
+     *         syntax constraints for mount points.
+     */
+    public MountPoint(URI uri, final boolean normalize)
+    throws URISyntaxException {
+        if (!uri.isAbsolute())
+            throw new URISyntaxException(uri.toString(), "Not absolute");
+        if (null != uri.getRawFragment())
+            throw new URISyntaxException(uri.toString(), "Fragment not allowed");
+        if (uri.isOpaque()) {
+            final String ssp = uri.getSchemeSpecificPart();
+            final int i = ssp.lastIndexOf(BANG_SLASH);
+            if (ssp.length() - 2 != i)
+                throw new URISyntaxException(uri.toString(),
+                        "Doesn't end with separator \"" + BANG_SLASH + '"');
+            path = new Path(new URI(ssp.substring(0, i)), normalize);
+            final URI pathUri = path.getUri();
+            if (!pathUri.isAbsolute())
+                throw new URISyntaxException(pathUri.toString(), "Not absolute");
+            final EntryName entryName = path.getEntryName();
+            if (null != entryName && 0 == entryName.toString().length())
+                throw new URISyntaxException(pathUri.toString(), "Empty entry name");
+            if (normalize) {
+                final URI nuri = new URI(
+                        uri.getScheme(), pathUri.toString() + BANG_SLASH, null);
+                uri = uri.equals(nuri) ? uri : nuri;
+            } else if (pathUri.normalize() != pathUri)
+                throw new URISyntaxException(pathUri.toString(),
+                        "Path not in normal form");
+        } else {
+            if (normalize)
+                uri = uri.normalize();
+            else if (uri.normalize() != uri)
+                throw new URISyntaxException(uri.toString(),
+                        "Path not in normal form");
+            path = null;
+        }
+        this.uri = uri;
+
+        assert invariants();
+    }
+
+    /**
+     * Constructs a new mount point by synthesizing its URI from the given
+     * scheme and path.
+     *
+     * @param  scheme the non-{@code null} {@link #getUri() URI} scheme.
+     * @param  path the non-{@code null} {@link #getPath() path}.
+     * @throws IllegalArgumentException if the synthesized mount point URI
+     *         would not conform to the syntax constraints for mount points.
+     * @return A non-{@code null} mount point.
+     */
+    public static MountPoint create(String scheme, Path path) {
+        try {
+            return new MountPoint(scheme, path);
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    /**
+     * Constructs a new mount point by synthesizing its URI from the given
+     * scheme and path.
+     *
+     * @param  scheme the non-{@code null} {@link #getUri() URI} scheme.
+     * @param  path the non-{@code null} {@link #getPath() path}.
+     * @throws URISyntaxException if the synthesized mount point URI
+     *         would not conform to the syntax constraints for mount points.
+     */
+    public MountPoint(final String scheme, final Path path)
+    throws URISyntaxException {
+        if (!isScheme(scheme))
+            throw new URISyntaxException(scheme, "Not a scheme");
+        final URI pathUri = path.getUri();
+        if (!pathUri.isAbsolute())
+            throw new URISyntaxException(pathUri.toString(), "Not absolute");
+        if (null != pathUri.getRawFragment())
+            throw new URISyntaxException(pathUri.toString(), "Fragment not allowed");
+        final EntryName entryName = path.getEntryName();
+        if (null != entryName && 0 == entryName.toString().length())
+            throw new URISyntaxException(pathUri.toString(), "Empty entry name in opaque path");
+        this.uri = new URI(scheme, path.toString() + BANG_SLASH, null);
+        this.path = path;
+
+        assert invariants();
+    }
+
+    /** Checks if the given string is a scheme according to RFC 2396. */
+    private static boolean isScheme(final String scheme) {
+        int i = scheme.length();
+        if (0 >= i)
+            return false;
+        char c;
+        c = scheme.charAt(0);
+        // TODO: Character class is no help here - consider table lookup!
+        if ((c < 'a' || 'z' < c) && (c < 'A' || 'Z' < c))
+            return false;
+        while (--i >= 1) {
+            c = scheme.charAt(i);
+            if ((c < 'a' || 'z' < c) && (c < 'A' || 'Z' < c)
+                    && c != '+' && c != '-' && c != '.')
+                return false;
+        }
+        return true;
+    }
+
+    private boolean invariants() {
+        assert null != uri;
+        assert uri.isAbsolute();
+        assert null == uri.getRawFragment();
+        if (uri.isOpaque()) {
+            assert uri.getRawSchemeSpecificPart().endsWith(BANG_SLASH);
+            assert null != path;
+            assert path.getUri().isAbsolute();
+            assert path.getUri().normalize() == path.getUri();
+            assert null == path.getUri().getRawFragment();
+            assert !path.getUri().isOpaque()
+                    || 0 < path.getEntryName().toString().length();
+        } else {
+            assert uri.normalize() == uri;
+            assert null == path;
+        }
+        return true;
+    }
+
+    /**
+     * Returns the path or {@code null} iff the {@link #getUri() URI}
+     * is hierarchical.
+     *
+     * @return The nullable path.
+     */
+    public Path getPath() {
+        return path;
+    }
+
+    /**
+     * Returns the non-{@code null} URI.
+     *
+     * @return The non-{@code null} URI.
+     */
+    public URI getUri() {
+        return uri;
+    }
+
+    /**
+     * Returns {@code true} iff the given object is a mount point and its URI
+     * {@link URI#equals(Object) equals} the URI of this mount point.
+     * Note that this ignores the scheme and path.
+     */
+    @Override
+    public boolean equals(final Object that) {
+        return this == that
+                || that instanceof MountPoint
+                    && this.uri.equals(((MountPoint) that).uri);
+    }
+
+    /**
+     * Implements a natural ordering which is consistent with
+     * {@link #equals(Object)}.
+     */
+    @Override
+    public int compareTo(final MountPoint that) {
+        return this.uri.compareTo(that.uri);
+    }
+
+    /**
+     * Returns a hash code which is consistent with {@link #equals(Object)}.
+     */
+    @Override
+    public int hashCode() {
+        return uri.hashCode();
+    }
+
+    /**
+     * Equivalent to calling {@link URI#toString()} on {@link #getUri()}.
+     */
+    @Override
+    public String toString() {
+        return uri.toString();
+    }
+}
