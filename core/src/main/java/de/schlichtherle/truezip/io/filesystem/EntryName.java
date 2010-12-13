aafd2c36be74e3fee597109a396668c57cd18f9e
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2010-12-13 21:32:41 +0100

    Revisioned paths.

--- core/src/main/java/de/schlichtherle/truezip/io/filesystem/EntryName.java
+++ core/src/main/java/de/schlichtherle/truezip/io/filesystem/EntryName.java
@@ -57,6 +57,7 @@ public final class EntryName implements Serializable, Comparable<EntryName> {
 
     private static final long serialVersionUID = 2212342253466752478L;
 
+    /** Represents an entry name with an empty URI. */
     static EntryName ROOT = EntryName.create(URI.create(FileSystemEntry.ROOT));
 
     private final URI uri;
