b1e68d052c8079610452656c49fd2c4c83c4d298
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-01-06 22:22:19 +0100

    Spinned of truezip-driver-tzp.

---
+++ tck/src/test/java/de/schlichtherle/truezip/io/rof/MemoryMappedReadOnlyFileTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2007-2010 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.schlichtherle.truezip.io.rof;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+
+/**
+ * @author Christian Schlichtherle
+ * @version $Id$
+ */
+public final class MemoryMappedReadOnlyFileTest extends ReadOnlyFileTestCase {
+
+    @Override
+    protected ReadOnlyFile newReadOnlyFile(File file) throws IOException {
+        return new MemoryMappedReadOnlyFile(file);
+    }
+
+    @Override
+    public void tearDown() throws IOException {
+        try {
+            super.tearDown();
+        } finally {
+            System.gc();
+            System.runFinalization();
+        }
+    }
+}
