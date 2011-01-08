c6309aa3c1afb66d244fc6f1528e745294b01fac
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-01-08 10:12:33 +0100

    Enhanced Javadoc.

--- kernel/src/main/java/de/schlichtherle/truezip/util/ServiceLocator.java
+++ kernel/src/main/java/de/schlichtherle/truezip/util/ServiceLocator.java
@@ -26,6 +26,21 @@ import java.util.ServiceLoader;
 /**
  * Provides utility methods for convenient class, resource and service location
  * which is designed to work in both OSGi and JEE environments.
+ * <p>
+ * If the class loader provided to the constructor is the current thread's
+ * context class loader, then the methods of this class will locate classes,
+ * resources or services by using this class loader only.
+ * Otherwise, the given class loader is used first. Second, the current
+ * thread's context class loader is used.
+ * <p>
+ * When loading a class, the search stops on the first hit, so if the given
+ * class loader finds the class, the current thread's context class loader is
+ * not used.
+ * <p>
+ * When enumerating resources and services, the results of both class loaders
+ * are concatenated, so a resource or a service may get enumerated twice!
+ * If this is not acceptable, you should create a set from the enumeration
+ * results.
  *
  * @author Christian Schlichtherle
  * @version $Id$
@@ -42,6 +57,14 @@ public final class ServiceLocator {
         this(null);
     }
 
+    /**
+     * Constructs a new service locator which uses the given class loader first
+     * to locate classes, resources or services on the class path.
+     * If this is {@code null}, then the system class loader is used.
+     *
+     * @param loader the nullable class loader to locate classes, resources and
+     *        services.
+     */
     public ServiceLocator(final @CheckForNull ClassLoader loader) {
         this.l1 = null != loader ? loader : ClassLoader.getSystemClassLoader();
     }
@@ -66,8 +89,8 @@ public final class ServiceLocator {
 
     /**
      * @param  name The resource to locate.
-     * @return A joint enumeration for the resource on the class path.
-     * @throws IOException If an I/O error occurs.
+     * @return A concatenated enumeration for the resource on the class path.
+     * @throws IOException If an I/O exception occurs.
      */
     public @NonNull Enumeration<URL> getResources(@NonNull String name)
     throws IOException {
@@ -80,8 +103,8 @@ public final class ServiceLocator {
 
     /**
      * @param  name The service to locate.
-     * @return A joint iteration for the service on the class path.
-     * @throws ServiceConfigurationError If the service cannot get located.
+     * @return A concatenated iteration for the service on the class path.
+     * @throws ServiceConfigurationError If an exception occurs.
      */
     public @NonNull <S> Iterator<S> getServices(@NonNull Class<S> service) {
         ClassLoader l2 = Thread.currentThread().getContextClassLoader();
