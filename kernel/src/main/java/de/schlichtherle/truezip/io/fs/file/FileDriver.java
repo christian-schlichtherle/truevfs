8c4b23c8aa53ff8507c4bc5af740caf52324a02c
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-01-10 06:27:37 +0100

    Changed driver loading.

---
+++ kernel/src/main/java/de/schlichtherle/truezip/io/fs/file/FileDriver.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schlichtherle.truezip.io.fs.file;
+
+import de.schlichtherle.truezip.io.fs.FsController;
+import de.schlichtherle.truezip.io.fs.FsDriver;
+import de.schlichtherle.truezip.io.fs.FsModel;
+import de.schlichtherle.truezip.io.fs.FsMountPoint;
+
+/**
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+public final class FileDriver implements FsDriver {
+
+    private static final long serialVersionUID = 4085765023564882687L;
+
+    @Override
+    public FsController<?>
+    newController(FsMountPoint mountPoint, FsController<?> parent) {
+        assert null == mountPoint.getParent()
+                ? null == parent
+                : mountPoint.getParent().equals(parent.getModel().getMountPoint());
+        if (null != parent)
+            throw new IllegalArgumentException();
+        return new FileController(new FsModel(mountPoint));
+    }
+}
