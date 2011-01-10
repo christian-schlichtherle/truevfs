5738fe517c96aa9ff4c32014f5291e39cc60112f
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-01-10 22:52:52 +0100

    Updated Javadoc.

--- driver/zip/src/test/java/de/schlichtherle/truezip/file/JarFileTest.java
+++ driver/zip/src/test/java/de/schlichtherle/truezip/file/JarFileTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010 Schlichtherle IT Services
+ * Copyright (C) 2009-2011 Schlichtherle IT Services
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
