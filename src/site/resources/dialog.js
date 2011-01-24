69d637c152761dcf6066de05cd41ffedbab9dc4b
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-01-24 19:56:42 +0100

    Updated site.

---
+++ src/site/resources/dialog.js
@@ -0,0 +1,9 @@
+function showDialog(id) {
+    if (document.getElementById)
+        document.getElementById(id).style.display='inline';
+}
+
+function closeDialog(id) {
+    if (document.getElementById)
+        document.getElementById(id).style.display='none';
+}
