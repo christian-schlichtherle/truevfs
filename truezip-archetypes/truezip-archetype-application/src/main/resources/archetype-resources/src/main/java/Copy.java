d488803f2440cc863ff084224c21fcdf1b769e69
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-04-27 05:05:32 +0200

    Updated archetype.

--- truezip-archetypes/truezip-archetype-application/src/main/resources/archetype-resources/src/main/java/Copy.java
+++ truezip-archetypes/truezip-archetype-application/src/main/resources/archetype-resources/src/main/java/Copy.java
@@ -24,6 +24,7 @@ import de.schlichtherle.truezip.file.TFile;
 //import de.schlichtherle.truezip.fs.archive.tar.TarDriver;
 //import de.schlichtherle.truezip.fs.archive.tar.TarGZipDriver;
 //import de.schlichtherle.truezip.fs.archive.zip.ZipDriver;
+import de.schlichtherle.truezip.fs.FsSyncException;
 //import de.schlichtherle.truezip.socket.sl.IOPoolLocator;
 import java.io.IOException;
 
@@ -42,23 +43,53 @@ import java.io.IOException;
  */
 public class Copy {
     public static void main(String[] args) throws IOException {
+        // A typical TrueZIP File* application life cycle contains of the
+        // following three phases:
+        setup(); // phase #1 (optional)
+        do {
+            try {
+                work(args); // phase #2
+            } finally {
+                // ALWAYS calls this in a finally-block after the work phase!
+                sync(); // phase #3
+            }
+        } while (false);
+        // The loop is redundant in this example and only kept to demonstrate
+        // the general procedure.
+    }
+
+    /**
+     * Runs the setup phase of this TrueZIP File* application.
+     * <p>
+     * The setup phase should be entered only once at the start of the
+     * application.
+     * Its task is to configure the File* API so that it recognizes the archive
+     * file suffixes which shall be treated like virtual directories.
+     * <p>
+     * Note that this phase is completely optional: If it's omitted, the File*
+     * module will automatically recognize the canonical archive file suffixes
+     * registered by any archive driver module which is present on the class
+     * path at run time - which can be configured by editing the pom.xml file.
+     */
+    private static void setup() {
         // The constructors of the TFile class use the ArchiveDetector
         // interface to scan a path name for suffixes of archive files
         // which shall be treated like virtual directories.
         // You can either explicitly inject this ArchiveDetector dependency
         // into a TFile constructor or you can set the following class
         // property to the default ArchiveDetector.
-        //   If you comment out the following statement, TrueZIP will
+        //   If you comment out the following statement, the File* module will
         // automatically recognize the canonical archive file suffixes
-        // registered by any archive driver module which is present on the run
-        // time class path - you can configure this using the pom.xml file.
+        // registered by any archive driver module which is present on the
+        // class path at run time - which can be configured by editing the
+        // pom.xml file.
         //   However, if you uncomment the following statement, you explicitly
         // state which file suffixes shall get recognized as archive files and
         // hence as virtual directories.
         //   The following statement registers the canonical archive file
         // suffixes for TAR, TAR.GZ, TAR.BZ2 and ZIP files and would require
         // the JARs for the archive driver modules truezip-driver-tar and
-        // truezip-driver-zip to be present on the compile time class path.
+        // truezip-driver-zip to be present on the class path at compile time:
         /*TFile.setDefaultArchiveDetector(
                 new TDefaultArchiveDetector(
                     TDefaultArchiveDetector.NULL,
@@ -68,21 +99,16 @@ public class Copy {
                         { "tbz|tb2|tar.bz2", new TarBZip2Driver(IOPoolLocator.SINGLETON) },
                         { "zip", new ZipDriver(IOPoolLocator.SINGLETON)},
                     }));*/
-
-        try {
-            copy(args);
-        } finally {
-            // Commits all unsynchronized changes to the contents of all
-            // federated file systems (i.e. archive files) to their respective
-            // parent file system.
-            //   In this context, this is an optional operation:
-            // When the JVM shuts down, it performs the same tasks as this
-            // method.
-            TFile.umount();
-        }
     }
 
-    private static void copy(String[] args) throws IOException {
+    /**
+     * Runs the work phase of this TrueZIP File* application.
+     * <p>
+     * The work phase may be executed several times based on some events
+     * triggered by the application.
+     * When it's finished, the {@link #sync() sync phase} must be started next.
+     */
+    private static void work(String[] args) throws IOException {
         // Setup the file operands.
         TFile src = new TFile(args[0]);
         TFile dst = new TFile(args[1]);
@@ -92,5 +118,27 @@ public class Copy {
         
         // Perform a recursive archive copy.
         TFile.cp_rp(src, dst);
+    }
+
+    /**
+     * Runs the sync phase of this TrueZIP File* application.
+     * <p>
+     * The sync phase commits all unsynchronized changes to the contents of all
+     * federated file systems (i.e. archive files) to their respective parent
+     * file system, releases these archive files for access by third parties,
+     * e.g. other processes, and cleans up any temporary files.
+     * Note that temporary files may get used even if the work phase solely
+     * executed read-only operations.
+     * <p>
+     * BTW: In the isolated context of this example, this is an optional
+     * operation: When the JVM shuts down, it performs the same tasks as this
+     * method.
+     */
+    private static void sync() throws FsSyncException {
+        // Avoid calling this method in a loop which updates the same archive
+        // file in each iteration: This would degrade the overall performance
+        // from O(n) to O(m*n), where m is the number of new or modified
+        // entries and n is the number of all entries in the archive!
+        TFile.umount();
     }
 }
