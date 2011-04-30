3dd4c47df8325658007ae39ff93020d1945eef8d
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-04-30 04:17:51 +0200

    Completed URI utility classes.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsScheme.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsScheme.java
@@ -15,6 +15,7 @@
  */
 package de.schlichtherle.truezip.fs;
 
+import de.schlichtherle.truezip.util.URIBuilder;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
@@ -68,26 +69,8 @@ public final class FsScheme implements Serializable, Comparable<FsScheme> {
      *         syntax constraints for URI schemes.
      */
     public FsScheme(final @NonNull String scheme) throws URISyntaxException {
-        final int l = scheme.length();
-        if (0 >= l)
-            throw new URISyntaxException(quote(scheme), "Empty URI scheme");
-        char c;
-        c = scheme.charAt(0);
-        // TODO: Character class is no help here - consider table lookup!
-        if ((c < 'a' || 'z' < c) && (c < 'A' || 'Z' < c))
-            throw new URISyntaxException(quote(scheme), "Illegal character in URI scheme", 0);
-        for (int i = 1; i < l; i++) {
-            c = scheme.charAt(i);
-            if ((c < 'a' || 'z' < c) && (c < 'A' || 'Z' < c)
-                    && (c < '0' || '9' < c)
-                    && c != '+' && c != '-' && c != '.')
-                throw new URISyntaxException(quote(scheme), "Illegal character in URI scheme", i);
-        }
+        URIBuilder.validateScheme(scheme);
         this.scheme = scheme;
-    }
-
-    private static String quote(Object s) {
-        return "\"" + s + "\"";
     }
 
     @Override
--- truezip-kernel/src/test/java/de/schlichtherle/truezip/util/URICodecTest.java
+++ truezip-kernel/src/test/java/de/schlichtherle/truezip/util/URICodecTest.java
@@ -16,12 +16,12 @@
 package de.schlichtherle.truezip.util;
 
 import java.util.logging.Logger;
-import de.schlichtherle.truezip.util.URICodec.Component;
+import de.schlichtherle.truezip.util.URICodec.Encoding;
 import java.util.EnumSet;
 import org.junit.Before;
 import org.junit.Test;
 
-import static de.schlichtherle.truezip.util.URICodec.Component.*;
+import static de.schlichtherle.truezip.util.URICodec.Encoding.*;
 import static java.util.logging.Level.*;
 import static org.junit.Assert.*;
 
@@ -88,34 +88,34 @@ public class URICodecTest {
     @Test
     public void testRoundTrip() {
         for (final Object[] test : new Object[][] {
-            { EnumSet.allOf(Component.class), "\ufffd", "%EF%BF%BD" }, // replacement character
-            { EnumSet.allOf(Component.class), "abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz" },
-            { EnumSet.allOf(Component.class), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
-            { EnumSet.allOf(Component.class), "0123456789", "0123456789" },
-            { EnumSet.allOf(Component.class), "_-!.~'()*", "_-!.~'()*" }, // mark
-            { EnumSet.allOf(Component.class), "@", "@" },
+            { EnumSet.allOf(Encoding.class), "\ufffd", "%EF%BF%BD" }, // replacement character
+            { EnumSet.allOf(Encoding.class), "abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz" },
+            { EnumSet.allOf(Encoding.class), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+            { EnumSet.allOf(Encoding.class), "0123456789", "0123456789" },
+            { EnumSet.allOf(Encoding.class), "_-!.~'()*", "_-!.~'()*" }, // mark
+            { EnumSet.allOf(Encoding.class), "@", "@" },
             { EnumSet.of(ANY, PATH), ":", "%3A" },
             { EnumSet.of(AUTHORITY, ABSOLUTE_PATH, QUERY, FRAGMENT), ":", ":" },
             { EnumSet.of(ANY, AUTHORITY), "/", "%2F" },
             { EnumSet.of(ABSOLUTE_PATH, PATH, QUERY, FRAGMENT), "/", "/" },
             { EnumSet.of(ANY, AUTHORITY, ABSOLUTE_PATH, PATH), "?", "%3F" },
             { EnumSet.of(QUERY, FRAGMENT), "?", "?" },
-            { EnumSet.allOf(Component.class), "#", "%23" },
-            { EnumSet.allOf(Component.class), "%", "%25" }, // percent
-            { EnumSet.allOf(Component.class), "%a", "%25a" }, // percent adjacent
-            { EnumSet.allOf(Component.class), "a%", "a%25" }, // percent adjacent
-            { EnumSet.allOf(Component.class), "%%", "%25%25" }, // percents
-            { EnumSet.allOf(Component.class), "a%b", "a%25b" }, // percent embedded
-            { EnumSet.allOf(Component.class), "%a%", "%25a%25" }, // reverse embedded
-            { EnumSet.allOf(Component.class), " ", "%20" },
-            { EnumSet.allOf(Component.class), "\u0000", "%00" }, // control
-            { EnumSet.allOf(Component.class), "\u20ac", "%E2%82%AC" }, // Euro sign
-            { EnumSet.allOf(Component.class), "a\u20acb", "a%E2%82%ACb" }, // dito embedded
-            { EnumSet.allOf(Component.class), "\u20aca\u20ac", "%E2%82%ACa%E2%82%AC" }, // inverse embedding
-            { EnumSet.allOf(Component.class), "\u00c4\u00d6\u00dc\u00df\u00e4\u00f6\u00fc", "%C3%84%C3%96%C3%9C%C3%9F%C3%A4%C3%B6%C3%BC" }, // German diaeresis and sharp s
-            { EnumSet.allOf(Component.class), "a\u00c4b\u00d6c\u00dcd\u00dfe\u00e4f\u00f6g\u00fch", "a%C3%84b%C3%96c%C3%9Cd%C3%9Fe%C3%A4f%C3%B6g%C3%BCh" }, // dito embedded
+            { EnumSet.allOf(Encoding.class), "#", "%23" },
+            { EnumSet.allOf(Encoding.class), "%", "%25" }, // percent
+            { EnumSet.allOf(Encoding.class), "%a", "%25a" }, // percent adjacent
+            { EnumSet.allOf(Encoding.class), "a%", "a%25" }, // percent adjacent
+            { EnumSet.allOf(Encoding.class), "%%", "%25%25" }, // percents
+            { EnumSet.allOf(Encoding.class), "a%b", "a%25b" }, // percent embedded
+            { EnumSet.allOf(Encoding.class), "%a%", "%25a%25" }, // reverse embedded
+            { EnumSet.allOf(Encoding.class), " ", "%20" },
+            { EnumSet.allOf(Encoding.class), "\u0000", "%00" }, // control
+            { EnumSet.allOf(Encoding.class), "\u20ac", "%E2%82%AC" }, // Euro sign
+            { EnumSet.allOf(Encoding.class), "a\u20acb", "a%E2%82%ACb" }, // dito embedded
+            { EnumSet.allOf(Encoding.class), "\u20aca\u20ac", "%E2%82%ACa%E2%82%AC" }, // inverse embedding
+            { EnumSet.allOf(Encoding.class), "\u00c4\u00d6\u00dc\u00df\u00e4\u00f6\u00fc", "%C3%84%C3%96%C3%9C%C3%9F%C3%A4%C3%B6%C3%BC" }, // German diaeresis and sharp s
+            { EnumSet.allOf(Encoding.class), "a\u00c4b\u00d6c\u00dcd\u00dfe\u00e4f\u00f6g\u00fch", "a%C3%84b%C3%96c%C3%9Cd%C3%9Fe%C3%A4f%C3%B6g%C3%BCh" }, // dito embedded
         }) {
-            for (final Component component : (EnumSet<Component>) test[0])
+            for (final Encoding component : (EnumSet<Encoding>) test[0])
                 assertEquals(test[2], codec.encode(test[1].toString(), component));
             assertEquals(test[1], codec.decode(test[2].toString()));
         }
