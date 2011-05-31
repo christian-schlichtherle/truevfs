9c4fac0dc30ef270ee0e72e60467d6f5c5220af9
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-06-01 01:06:36 +0200

    Cleanup.

--- truezip-drivers/truezip-driver-file-jse7/src/main/java/de/schlichtherle/truezip/fs/file/nio/TempFilePool.java
+++ truezip-drivers/truezip-driver-file-jse7/src/main/java/de/schlichtherle/truezip/fs/file/nio/TempFilePool.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import static java.nio.file.Files.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.rmi.UnexpectedException;
 import net.jcip.annotations.NotThreadSafe;
 import net.jcip.annotations.ThreadSafe;
 
