2dcc8319cd1a4ff5a209c8c282d6aa497574fc63
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-07-29 15:34:50 +0200

    Introduced annotations.

--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/zip/UShort.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/zip/UShort.java
@@ -16,15 +16,20 @@
 
 package de.schlichtherle.truezip.zip;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * Provides constants and static utility methods for unsigned short integer
  * values ({@value SIZE} bits).
- * <p>
- * This class is safe for multithreading.
  * 
- * @author Christian Schlichtherle
+ * @author  Christian Schlichtherle
  * @version $Id$
  */
+@ThreadSafe
+@DefaultAnnotation(NonNull.class)
 final class UShort {
 
     /**
@@ -65,17 +70,17 @@ final class UShort {
      */
     public static void check(
             final int i,
-            final String subject,
-            final String error) {
+            final @CheckForNull String subject,
+            final @CheckForNull String error) {
         if (MIN_VALUE <= i && i <= MAX_VALUE)
             return;
 
         final StringBuilder message = new StringBuilder();
-        if (subject != null) {
+        if (null != subject) {
             message.append(subject);
             message.append(": ");
         }
-        if (error != null) {
+        if (null != error) {
             message.append(error);
             message.append(": ");
         }
