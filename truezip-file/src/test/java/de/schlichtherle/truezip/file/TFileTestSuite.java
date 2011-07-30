a2a7b70180960bc98159de79567dcadc25bd8217
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-07-30 13:22:18 +0200

    RCB.

--- truezip-file/src/test/java/de/schlichtherle/truezip/file/TFileTestSuite.java
+++ truezip-file/src/test/java/de/schlichtherle/truezip/file/TFileTestSuite.java
@@ -26,7 +26,6 @@ import de.schlichtherle.truezip.socket.IOPoolProvider;
 import de.schlichtherle.truezip.socket.OutputClosedException;
 import de.schlichtherle.truezip.socket.spi.ByteArrayIOPoolService;
 import de.schlichtherle.truezip.util.ArrayHelper;
-import de.schlichtherle.truezip.util.BitField;
 import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.ByteArrayInputStream;
