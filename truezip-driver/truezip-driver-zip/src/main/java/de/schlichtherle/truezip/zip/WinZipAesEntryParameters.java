c5474c4fa1eb58a88f00f2130c75530a8e9ffcd2
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-08-14 03:38:30 +0200

    Improved processing of WinZip AES entries.

--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/zip/WinZipAesEntryParameters.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/zip/WinZipAesEntryParameters.java
@@ -62,10 +62,4 @@ final class WinZipAesEntryParameters {
     byte[] getReadPassword(boolean invalid) throws ZipKeyException {
         return param.getReadPassword(entry.getName(), invalid);
     }
-
-    int getOverhead() throws ZipKeyException {
-        return getKeyStrength().getBytes() / 2 // salt value
-                + 2   // password verification value
-                + 10; // authentication code
-    }
 }
