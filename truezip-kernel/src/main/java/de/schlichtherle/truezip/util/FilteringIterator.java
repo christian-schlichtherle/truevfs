3079dfd9a934a0c2158b454a366c558f299ab54b
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-08-16 19:53:22 +0200

    Added FilteringIterator.

---
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/util/FilteringIterator.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2011 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schlichtherle.truezip.util;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * An iterator which filters another iterator.
+ * 
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+@NotThreadSafe
+@DefaultAnnotation(NonNull.class)
+public abstract class FilteringIterator<T> implements Iterator<T> {
+    private final Iterator<T> iterator;
+    private Boolean hasNext;
+    private @CheckForNull T next;
+
+    /**
+     * Constructs a new filtering iterator which filters the given decorator.
+     * 
+     * @param iterator the iterator to filter.
+     */
+    protected FilteringIterator(final Iterator<T> iterator) {
+        assert null != iterator; // doesn't matter much if assertions are disabled
+        this.iterator = iterator;
+    }
+
+    private void peek() {
+        if (null != hasNext)
+            return;
+        while (iterator.hasNext()) {
+            if (accept(next = iterator.next())) {
+                hasNext = true;
+                return;
+            }
+        }
+        hasNext = false;
+    }
+
+    /**
+     * Returns {@code true} if and only if this filtering iterator accepts the
+     * given element.
+     * 
+     * @param  element the element to test
+     * @return {@code true} if and only if this filtering iterator accepts the
+     *         given element.
+     */
+    protected abstract boolean accept(@Nullable T element);
+
+    @Override
+    public boolean hasNext() {
+        peek();
+        return hasNext;
+    }
+
+    @Override
+    public T next() {
+        peek();
+        if (!hasNext)
+            throw new NoSuchElementException();
+        hasNext = null;
+        return next;
+    }
+
+    @Override
+    public void remove() {
+        iterator.remove();
+    }
+}
