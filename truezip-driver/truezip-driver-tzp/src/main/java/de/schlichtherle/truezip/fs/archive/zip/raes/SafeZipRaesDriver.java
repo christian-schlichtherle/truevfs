55b2e0585fa91a0bce4c8a4ea496dffaafc482f5
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-09-07 12:33:25 +0200

    RCB.

--- truezip-driver/truezip-driver-tzp/src/main/java/de/schlichtherle/truezip/fs/archive/zip/raes/SafeZipRaesDriver.java
+++ truezip-driver/truezip-driver-tzp/src/main/java/de/schlichtherle/truezip/fs/archive/zip/raes/SafeZipRaesDriver.java
@@ -12,7 +12,6 @@ import de.schlichtherle.truezip.key.KeyManagerProvider;
 import de.schlichtherle.truezip.socket.IOPoolProvider;
 import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 import java.io.IOException;
 import net.jcip.annotations.Immutable;
 
