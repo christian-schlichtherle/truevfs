4996c4f9f5809aa8a14dc2e2c96d106e3d903f7f
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-09-08 13:25:32 +0200

    Updated documentation.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/socket/OutputSocket.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/socket/OutputSocket.java
@@ -19,7 +19,7 @@ import java.nio.channels.SeekableByteChannel;
 import net.jcip.annotations.NotThreadSafe;
 
 /**
- * An abstract factory for output streams for writing bytes to its
+ * An abstract factory for output resources for writing bytes to its
  * <i>local target</i>.
  * <p>
  * Note that the entity relationship between output sockets and input sockets
@@ -53,17 +53,18 @@ extends IOSocket<E, Entry> {
     }
 
     /**
-     * Binds the peer target of the given socket to this socket.
+     * Binds this socket to given socket by inheriting its
+     * {@link #getPeerTarget() peer target}.
      * Note that this method does <em>not</em> change the state of the
      * given socket and does <em>not</em> connect this socket to the peer
-     * socket, i.e. it does not set this socket as the peer of of the given
+     * socket, that is it does not set this socket as the peer of of the given
      * socket.
      *
-     * @param  to the output socket which has a peer target to share.
+     * @param  to the output socket with the peer target to inherit.
      * @return {@code this}
      */
     public final OutputSocket<E> bind(@CheckForNull final OutputSocket<?> to) {
-        peer = null == to ? null : to.peer;
+        this.peer = null == to ? null : to.peer;
         return this;
     }
 
