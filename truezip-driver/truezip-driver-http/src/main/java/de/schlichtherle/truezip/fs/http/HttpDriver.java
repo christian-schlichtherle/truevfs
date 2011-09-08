531d42404a15195bace9f70d97a8c79c68ff1a01
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2011-09-08 17:22:35 +0200

    Introduced Apache HttpClient.

--- truezip-driver/truezip-driver-http/src/main/java/de/schlichtherle/truezip/fs/http/HttpDriver.java
+++ truezip-driver/truezip-driver-http/src/main/java/de/schlichtherle/truezip/fs/http/HttpDriver.java
@@ -16,19 +16,28 @@ import de.schlichtherle.truezip.socket.IOPoolProvider;
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
+import java.net.URI;
 import net.jcip.annotations.Immutable;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * A file system driver for the HTTP(S) schemes.
  * 
+ * @see     #newClient()
  * @author  Christian Schlichtherle
  * @version $Id$
  */
 @Immutable
 @DefaultAnnotation(NonNull.class)
-public final class HttpDriver extends FsDriver {
+public class HttpDriver extends FsDriver {
 
     private final IOPoolProvider provider;
+    private volatile @CheckForNull HttpClient client;
 
     public HttpDriver(final IOPoolProvider provider) {
         if (null == provider)
@@ -38,6 +47,46 @@ public final class HttpDriver extends FsDriver {
 
     IOPool<?> getPool() {
         return provider.get();
+    }
+
+    /**
+     * Returns the cached http client obtained by calling {@link #newClient()}.
+     * 
+     * @return The cached http client obtained by calling {@link #newClient()}.
+     */
+    protected final HttpClient getClient() {
+        final HttpClient client = this.client;
+        return null != client ? client : (this.client = newClient());
+    }
+
+    /**
+     * Returns a new http client.
+     * <p>
+     * The implementation in the class {@link HttpDriver} simply returns
+     * {@code new DefaultHttpClient()}.
+     * If you need special configuration, e.g. for authentication or caching,
+     * then you should override this method.
+     * 
+     * @return A new http client.
+     */
+    protected HttpClient newClient() {
+        return new DefaultHttpClient();
+    }
+
+    /**
+     * Executes the HEAD request method for the given URI.
+     * Equivalent to {@code getClient().execute(new HttpHead(uri))}.
+     */
+    protected HttpResponse executeHead(URI uri) throws IOException {
+        return getClient().execute(new HttpHead(uri));
+    }
+
+    /**
+     * Executes the GET request method for the given URI.
+     * Equivalent to {@code getClient().execute(new HttpGet(uri))}.
+     */
+    protected HttpResponse executeGet(URI uri) throws IOException {
+        return getClient().execute(new HttpGet(uri));
     }
 
     @Override
