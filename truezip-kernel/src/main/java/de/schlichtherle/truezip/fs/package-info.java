57867b6ab3cf0eb39098dd8458cf201def13e645
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-12-29 16:17:23 +0100

    Updated Javadoc.

---
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2004-2011 Schlichtherle IT Services
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Provides access to (possibly federated virtual) file systems.
+ * This package is the nexus between file system applications and file system
+ * drivers.
+ */
+package de.schlichtherle.truezip.fs;
