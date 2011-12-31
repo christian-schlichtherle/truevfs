8f69c8262da8afb4aa14924f976e57f47931001b
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2011-12-31 16:13:46 +0100

    RCB.

--- truezip-archetype/truezip-archetype-path/src/main/resources/archetype-resources/src/main/java/java/Tree.java
+++ truezip-archetype/truezip-archetype-path/src/main/resources/archetype-resources/src/main/java/java/Tree.java
@@ -6,7 +6,6 @@ package ${package}.java;
 import de.schlichtherle.truezip.fs.FsSyncException;
 import de.schlichtherle.truezip.nio.file.TPath;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import static java.nio.file.Files.*;
