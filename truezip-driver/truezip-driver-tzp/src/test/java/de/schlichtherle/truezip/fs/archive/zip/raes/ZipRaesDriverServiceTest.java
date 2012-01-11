754901eeb4333dd00cbeb44489136e8c921d09af
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-01-11 15:36:18 +0100

    RCB.

--- truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/fs/archive/zip/raes/ZipRaesDriverServiceTest.java
+++ truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/fs/archive/zip/raes/ZipRaesDriverServiceTest.java
@@ -8,14 +8,14 @@
  */
 package de.schlichtherle.truezip.fs.archive.zip.raes;
 
-import de.schlichtherle.truezip.fs.spi.FsDriverService;
 import de.schlichtherle.truezip.fs.FsScheme;
+import de.schlichtherle.truezip.fs.spi.FsDriverService;
 import de.schlichtherle.truezip.util.SuffixSet;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
-import static org.hamcrest.CoreMatchers.*;
 
 /**
  * @author  Christian Schlichtherle
