8a1484c414bbc2d0b686226eba2979080ad19a52
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-01-20 01:11:35 +0100

    Enhanced resource accounting.

---
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsCurrentThreadIOBusyException.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2004-2012 Schlichtherle IT Services
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.schlichtherle.truezip.fs;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * @since   TrueZIP 7.4.4
+ * @see     FsResourceController
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+@Immutable
+public final class FsCurrentThreadIOBusyException extends FsResourceIOBusyException {
+    private static final long serialVersionUID = 1L;
+
+    FsCurrentThreadIOBusyException(final int local) {
+        super("Number of open I/O streams for the current thread: %d", local);
+    }
+}
---
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsThreadsIOBusyException.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2004-2012 Schlichtherle IT Services
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.schlichtherle.truezip.fs;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * @since   TrueZIP 7.4.4
+ * @see     FsResourceController
+ * @author  Christian Schlichtherle
+ * @version $Id$
+ */
+@Immutable
+public final class FsThreadsIOBusyException extends FsResourceIOBusyException {
+    private static final long serialVersionUID = 1L;
+
+    FsThreadsIOBusyException(final int total, final int local) {
+        super("Total (thread local) number of open I/O streams: %d (%d)", total, local);
+    }
+}
