1c2fca6e16de8aa031b2425bd8be1f4ed913a7a1
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-01-29 20:15:48 +0100

    Changed to a subclass of IOException again.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsControllerException.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsControllerException.java
@@ -8,8 +8,6 @@
  */
 package de.schlichtherle.truezip.fs;
 
-import de.schlichtherle.truezip.socket.InputSocket;
-import de.schlichtherle.truezip.socket.OutputSocket;
 import java.io.IOException;
 import net.jcip.annotations.Immutable;
 
@@ -48,7 +46,7 @@ import net.jcip.annotations.Immutable;
  * <p>
  * As an implementor of a file system controller however, for example when
  * writing a custom controller for an archive file system driver by extending
- * this class, then you need to be aware that you may receive file system
+ * this class,  you need to be aware that you may receive file system
  * controller exceptions whenever you call a method on the decorated file
  * system controller.
  * Unless you have special requirements, you don't need to catch such an
@@ -67,36 +65,6 @@ import net.jcip.annotations.Immutable;
  *     }
  * }
  * }</pre>
- *
- * <h3>Why is this a sub-class of a {@code RuntimeException}?</h3>
- * <p>
- * This seems scary because it's too easy to miss it.
- * However, there are some reasons to this:
- * <ol>
- * <li>
- * The only alternative would have been an {@link IOException}.
- * It couldn't be anything else because this exception type may be thrown by
- * the methods of any {@link InputSocket} or {@link OutputSocket} created by
- * {@link FsDecoratingController#getInputSocket FsDecoratingController.getInputSocket()}
- * or
- * {@link FsDecoratingController#getOutputSocket FsDecoratigController.getOutputSocket()}
- * respectively, too.
- * <li>
- * Sub-classing {@code IOException} calls for trouble:
- * Most controller classes catch this exception type and apply special
- * treatment for good reason.
- * However, a controller exception is used for control flow and does
- * <em>not</em> necessarily indicate an I/O failure, which is a completely
- * different aspect!
- * So the logic to deal with it as an {@code IOException} would very likely be
- * wrong.
- * I faced this issue when a controller exception meaning to indicate a
- * potential dead lock was incorrectly wrapped in an {@link FsSyncException}
- * because it happened during a {@linkplain FsController#sync sync} operation
- * and was subsequently wrapped in another controller exception to indicate
- * a false positive archive file.
- * This treatment was obviously completely wrong.
- * </ol>
  * 
  * @see     FsDecoratingController
  * @since   TrueZIP 7.5 (renamed from {@code FsException} and changed super
@@ -106,7 +74,7 @@ import net.jcip.annotations.Immutable;
  */
 @Immutable
 @SuppressWarnings("serial") // serializing an exception for a temporary event is nonsense!
-public abstract class FsControllerException extends RuntimeException {
+public abstract class FsControllerException extends IOException {
     FsControllerException() {
     }
 
