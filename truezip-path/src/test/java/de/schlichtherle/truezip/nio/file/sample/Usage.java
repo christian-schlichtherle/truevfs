30b59615075cce1f955bfa18556532ea6dbed375
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-01-31 23:40:20 +0100

    Fixed annotations.

--- truezip-path/src/test/java/de/schlichtherle/truezip/nio/file/sample/Usage.java
+++ truezip-path/src/test/java/de/schlichtherle/truezip/nio/file/sample/Usage.java
@@ -14,6 +14,8 @@ import de.schlichtherle.truezip.io.SequentialIOException;
 import de.schlichtherle.truezip.io.Streams;
 import de.schlichtherle.truezip.nio.file.TFileSystemProvider;
 import de.schlichtherle.truezip.nio.file.TPath;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -30,6 +32,7 @@ import java.nio.file.Path;
  */
 @Deprecated
 @SuppressWarnings("CallToThreadDumpStack")
+@DefaultAnnotation(NonNull.class)
 abstract class Usage {
 
     /** Nope! */
