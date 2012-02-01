b9441b323e3893d2873282f606a7c5099ac94a57
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-02-01 19:01:18 +0100

    Updated documentation.

---
+++ truezip-extension/truezip-extension-jmx-jul/src/main/java/de/schlichtherle/truezip/fs/inst/comp/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2004-2012 Schlichtherle IT Services
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Provides locatable service providers for an instrumented file system manager
+ * and an I/O buffer pool.
+ */
+@DefaultAnnotation(NonNull.class)
+package de.schlichtherle.truezip.fs.inst.comp;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
