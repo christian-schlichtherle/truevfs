8784d8a59178442ce8011ff49191cf8df2d2081c
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-02-02 19:46:11 +0100

    Fixed annotations.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/io/IOExceptionOutputStream.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/io/IOExceptionOutputStream.java
@@ -9,8 +9,6 @@
 package de.schlichtherle.truezip.io;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
-import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import java.io.IOException;
 import java.io.OutputStream;
