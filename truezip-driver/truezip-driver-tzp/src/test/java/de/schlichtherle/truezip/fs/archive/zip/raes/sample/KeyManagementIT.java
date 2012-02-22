f9878bb9da66b192cbedd43808d586418ee6fa32
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-22 18:51:30 +0100

    Increased test stability.

--- truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/fs/archive/zip/raes/sample/KeyManagementIT.java
+++ truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/fs/archive/zip/raes/sample/KeyManagementIT.java
@@ -10,71 +10,15 @@ package de.schlichtherle.truezip.fs.archive.zip.raes.sample;
 
 import de.schlichtherle.truezip.file.TArchiveDetector;
 import de.schlichtherle.truezip.file.TFile;
-import de.schlichtherle.truezip.fs.FsSyncException;
-import static de.schlichtherle.truezip.fs.archive.zip.sample.KeyManagementIT.roundTripTest;
-import java.io.File;
+import de.schlichtherle.truezip.fs.archive.zip.sample.KeyManagementTestBase;
 import java.io.IOException;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.junit.After;
-import static org.junit.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author  Christian Schlichtherle
  * @version $Id$
  */
-public final class KeyManagementIT {
-
-    private static final Logger logger = Logger.getLogger(
-            KeyManagementIT.class.getName());
-
-    private static final String PREFIX = "tzp";
-    private static final String SUFFIX = "eaff";
-    private static final String PASSWORD = "secret";
-
-    private static final Random rnd = new Random();
-
-    /** The data to get compressed. */
-    private static final byte[] DATA = new byte[1024]; // enough to waste some heat on CPU cycles
-    static {
-        rnd.nextBytes(DATA);
-    }
-
-    private File temp;
-    private byte[] data;
-
-    @Before
-    public void setUp() throws IOException {
-        TFile.umount();
-        temp = createTempFile();
-        assertTrue(temp.delete());
-        data = DATA.clone();
-    }
-
-    private static File createTempFile() throws IOException {
-        // TODO: Removing .getCanonicalFile() may cause archive.rm_r() to
-        // fail in some cases - explain why!
-        // For more info, refer to TFileTestSuite.
-        return File.createTempFile(PREFIX, "." + SUFFIX).getCanonicalFile();
-    }
-
-    @After
-    public void tearDown() {
-        // sync now to delete temps and free memory.
-        // This prevents subsequent warnings about left over temporary files
-        // and removes cached data from the memory, so it helps to start on a
-        // clean sheet of paper with subsequent tests.
-        try {
-            TFile.umount();
-        } catch (FsSyncException ex) {
-            logger.log(Level.WARNING, ex.toString(), ex);
-        }
-        if (null != temp && temp.exists() && !temp.delete())
-            logger.log(Level.WARNING, "{0} (could not delete)", temp);
-    }
+public final class KeyManagementIT extends KeyManagementTestBase {
 
     @Test
     public void testSetPasswords1() throws IOException {
--- truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/fs/archive/zip/sample/KeyManagementIT.java
+++ truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/fs/archive/zip/sample/KeyManagementIT.java
@@ -10,64 +10,18 @@ package de.schlichtherle.truezip.fs.archive.zip.sample;
 
 import de.schlichtherle.truezip.file.TArchiveDetector;
 import de.schlichtherle.truezip.file.TFile;
-import de.schlichtherle.truezip.file.TFileInputStream;
-import de.schlichtherle.truezip.file.TFileOutputStream;
-import java.io.*;
+import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Random;
-import org.junit.After;
-import static org.junit.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author  Christian Schlichtherle
  * @version $Id$
  */
-public final class KeyManagementIT {
+public final class KeyManagementIT extends KeyManagementTestBase {
 
-    private static final String PREFIX = "tzp";
-    private static final String SUFFIX = "eaff";
-    private static final String PASSWORD = "secret";
     private static final Charset US_ASCII = Charset.forName("US-ASCII");
-
-    private static final Random rnd = new Random();
-
-    /** The data to get compressed. */
-    private static final byte[] DATA = new byte[1024]; // enough to waste some heat on CPU cycles
-    static {
-        rnd.nextBytes(DATA);
-    }
-
-    private File temp;
-    private byte[] data;
-
-    @Before
-    public void setUp() throws IOException {
-        TFile.umount();
-        temp = createTempFile();
-        assertTrue(temp.delete());
-        data = DATA.clone();
-    }
-
-    private static File createTempFile() throws IOException {
-        // TODO: Removing .getCanonicalFile() may cause archive.rm_r() to
-        // fail in some cases - explain why!
-        // For more info, refer to TFileTestSuite.
-        return File.createTempFile(PREFIX, "." + SUFFIX).getCanonicalFile();
-    }
-
-    @After
-    public void tearDown() throws IOException {
-        try {
-            TFile.umount();
-        } finally {
-            if (temp.exists() && !temp.delete())
-                throw new IOException(temp + " (could not delete)");
-        }
-    }
-
+    
     @Test
     public void testSetPasswords1() throws IOException {
         TArchiveDetector detector = KeyManagement.newArchiveDetector1(
@@ -84,24 +38,5 @@ public final class KeyManagementIT {
                 SUFFIX,
                 PASSWORD.toCharArray());
         roundTripTest(new TFile(temp, detector), data);
-    }
-
-    public static void roundTripTest(TFile archive, byte[] data)
-    throws IOException {
-        TFile file = new TFile(archive, "entry");
-        OutputStream out = new TFileOutputStream(file);
-        try {
-            out.write(data);
-        } finally {
-            out.close();
-        }
-        out = new ByteArrayOutputStream(data.length);
-        InputStream in = new TFileInputStream(file);
-        try {
-            TFile.cat(in, out);
-        } finally {
-            in.close();
-        }
-        Arrays.equals(data, ((ByteArrayOutputStream) out).toByteArray());
     }
 }
