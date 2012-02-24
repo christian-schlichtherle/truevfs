6b4c85001007b71f8918b582d2c7b32ea3dd7b41
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-24 19:39:40 +0100

    Made FsControllerException traceable upon request by the boolean system property de.schlichtherle.truezip.fs.FsControllerException.traceable .

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsNeedsSyncException.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsNeedsSyncException.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs;
 
@@ -16,14 +12,17 @@ import javax.annotation.concurrent.Immutable;
  * {@linkplain FsController#sync(BitField) synced} before the operation can
  * proceed.
  *
- * @since   TrueZIP 7.3
- * @see     FsSyncController
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @since  TrueZIP 7.3
+ * @see    FsSyncController
+ * @author Christian Schlichtherle
  */
 @Immutable
 @SuppressWarnings("serial") // serializing an exception for a temporary event is nonsense!
 public final class FsNeedsSyncException extends FsControllerException {
-    public static final FsNeedsSyncException
-            SINGLETON = new FsNeedsSyncException();
+    public static FsNeedsSyncException get() {
+        return TRACE ? new FsNeedsSyncException() : SINGLETON;
+    }
+
+    private static final FsNeedsSyncException
+            SINGLETON = TRACE ? null : new FsNeedsSyncException();
 }
--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsNeedsWriteLockException.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsNeedsWriteLockException.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs;
 
@@ -14,13 +10,16 @@ import javax.annotation.concurrent.Immutable;
  * Indicates that the file system needs to get write locked before the
  * operation can proceed.
  *
- * @see     FsLockController
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @see    FsLockController
+ * @author Christian Schlichtherle
  */
 @Immutable
 @SuppressWarnings("serial") // serializing an exception for a temporary event is nonsense!
 public final class FsNeedsWriteLockException extends FsControllerException {
-    static final FsNeedsWriteLockException
-            SINGLETON = new FsNeedsWriteLockException();
+    static FsNeedsWriteLockException get() {
+        return TRACE ? new FsNeedsWriteLockException() : SINGLETON;
+    }
+
+    private static final FsNeedsWriteLockException
+            SINGLETON = TRACE ? null : new FsNeedsWriteLockException();
 }
