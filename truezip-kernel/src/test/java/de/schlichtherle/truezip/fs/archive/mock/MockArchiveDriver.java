47f2cd5ac62e078c58be98b83289d8c1a8a7eaae
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-24 23:35:25 +0100

    RCB

--- truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/mock/MockArchiveDriver.java
+++ truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/mock/MockArchiveDriver.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive.mock;
 
@@ -28,8 +24,7 @@ import javax.annotation.CheckForNull;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 @ThreadSafe
 public class MockArchiveDriver
