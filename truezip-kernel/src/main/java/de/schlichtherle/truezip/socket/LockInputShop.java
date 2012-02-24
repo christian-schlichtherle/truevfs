9c093dd19a1ba190a6e4e8d0d2ccf0fb90889ace
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-24 17:02:51 +0100

    RCB

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/socket/LockInputShop.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/socket/LockInputShop.java
@@ -27,7 +27,7 @@ import javax.annotation.concurrent.ThreadSafe;
  * Decorates another input shop to allow concurrent access which is
  * synchronized by a {@link Lock} object provided to its constructor.
  *
- * @param   <E> The type of the entries.
+ * @param   <E> the type of the entries.
  * @see     LockOutputShop
  * @since   TrueZIP 7.5
  * @author  Christian Schlichtherle
