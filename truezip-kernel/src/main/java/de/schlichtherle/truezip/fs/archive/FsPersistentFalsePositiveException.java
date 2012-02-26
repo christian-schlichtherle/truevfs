dd6494bfa8c6da1dd35292b23d240d61c0bc140f
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-27 00:28:11 +0100

    RCB

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/archive/FsPersistentFalsePositiveException.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/archive/FsPersistentFalsePositiveException.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive;
 
@@ -24,8 +20,7 @@ import javax.annotation.concurrent.Immutable;
  * operations to the parent file system of a false positive federated (archive)
  * file system.
  * 
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 @Immutable
 @SuppressWarnings("serial") // serializing an exception for a temporary event is nonsense!
