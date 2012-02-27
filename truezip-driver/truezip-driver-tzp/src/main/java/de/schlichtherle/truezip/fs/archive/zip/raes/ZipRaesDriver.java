914fc351ab2533decc484a27ba6e4dce25ebebe4
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-02-27 13:23:14 +0100

    RCB

--- truezip-driver/truezip-driver-tzp/src/main/java/de/schlichtherle/truezip/fs/archive/zip/raes/ZipRaesDriver.java
+++ truezip-driver/truezip-driver-tzp/src/main/java/de/schlichtherle/truezip/fs/archive/zip/raes/ZipRaesDriver.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive.zip.raes;
 
@@ -40,8 +36,7 @@ import javax.annotation.concurrent.Immutable;
  * <p>
  * Sub-classes must be thread-safe and should be immutable!
  * 
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 @Immutable
 public abstract class ZipRaesDriver extends JarDriver {
