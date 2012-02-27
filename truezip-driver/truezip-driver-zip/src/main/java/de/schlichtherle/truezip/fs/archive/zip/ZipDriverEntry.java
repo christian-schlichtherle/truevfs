d694f15253c31b6d12a05779dcc5b2a7104bcced
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-02-27 02:53:16 +0100

    RCB

--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/ZipDriverEntry.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/ZipDriverEntry.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive.zip;
 
@@ -21,10 +17,9 @@ import de.schlichtherle.truezip.zip.ZipEntry;
  * ZIP archive entries apply the date/time conversion rules as defined by
  * {@link DateTimeConverter#ZIP}.
  *
- * @see     #getDateTimeConverter()
- * @see     ZipDriver
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @see    #getDateTimeConverter()
+ * @see    ZipDriver
+ * @author Christian Schlichtherle
  */
 public class ZipDriverEntry extends ZipEntry implements FsArchiveEntry {
 
