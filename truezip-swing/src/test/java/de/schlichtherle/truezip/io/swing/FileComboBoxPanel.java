e3d19bd0819419d97396d429dc8165c72dc3762b
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-01 03:01:37 +0100

    RCB

--- truezip-swing/src/test/java/de/schlichtherle/truezip/io/swing/FileComboBoxPanel.java
+++ truezip-swing/src/test/java/de/schlichtherle/truezip/io/swing/FileComboBoxPanel.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.io.swing;
 
@@ -13,8 +9,7 @@ import javax.annotation.CheckForNull;
 import javax.swing.JPanel;
 
 /**
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 public class FileComboBoxPanel extends JPanel {
     private static final long serialVersionUID = 1065812374938719922L;
