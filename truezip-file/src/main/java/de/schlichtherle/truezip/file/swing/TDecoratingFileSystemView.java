94ffe7b059c2ec47ad1209e19cb9988bd8f9de33
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-01 03:25:39 +0100

    RCB

--- truezip-file/src/main/java/de/schlichtherle/truezip/file/swing/TDecoratingFileSystemView.java
+++ truezip-file/src/main/java/de/schlichtherle/truezip/file/swing/TDecoratingFileSystemView.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.file.swing;
 
@@ -17,7 +13,6 @@ import javax.swing.filechooser.FileSystemView;
  * A decorator for a file system view.
  *
  * @author Christian Schlichtherle
- * @version $Id$
  */
 public abstract class TDecoratingFileSystemView extends FileSystemView {
 
