7b10967d6d43f86519e904a5315ba115b7b06f90
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-02 04:06:49 +0100

    updated

--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/ZipDriver.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/ZipDriver.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive.zip;
 
@@ -65,8 +61,7 @@ import javax.annotation.concurrent.Immutable;
  * <p>
  * Sub-classes must be thread-safe and should be immutable!
  *
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 @Immutable
 public class ZipDriver
--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/package-info.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/fs/archive/zip/package-info.java
@@ -1,13 +1,11 @@
 /*
- * Copyright (C) 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 /**
  * The archive driver family for ZIP files.
+ * 
+ * @author Christian Schlichtherle
  */
 @edu.umd.cs.findbugs.annotations.DefaultAnnotation(edu.umd.cs.findbugs.annotations.NonNull.class)
 package de.schlichtherle.truezip.fs.archive.zip;
