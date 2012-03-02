f9991d2b87bf9192703eb2024b24ad0a5ddca0bf
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-02 06:29:05 +0100

    RCB

--- truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/FsArchiveDriverTestBase.java
+++ truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/FsArchiveDriverTestBase.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive;
 
@@ -17,9 +13,8 @@ import org.junit.After;
 import org.junit.Before;
 
 /**
- * @param   <D> The type of the archive driver.
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @param  <D> The type of the archive driver.
+ * @author Christian Schlichtherle
  */
 public abstract class FsArchiveDriverTestBase<D extends FsArchiveDriver<?>> {
 
