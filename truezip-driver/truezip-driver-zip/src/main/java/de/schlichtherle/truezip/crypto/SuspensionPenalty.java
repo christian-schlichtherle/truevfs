18ca8c4bc00c61e69b12564c14dd7e0cee3ef0ca
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-03 04:24:13 +0100

    Using new Threads class.

--- truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/crypto/SuspensionPenalty.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/schlichtherle/truezip/crypto/SuspensionPenalty.java
@@ -1,32 +1,30 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.crypto;
+
+import de.schlichtherle.truezip.util.Threads;
+import javax.annotation.concurrent.Immutable;
 
 /**
  * A utility class for enforcing a suspension penalty of at least
  * {@link #MIN_KEY_RETRY_DELAY} milliseconds between two subsequent key
  * verifications.
  * 
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
+@Immutable
 public class SuspensionPenalty {
-
-    /* can't touch this - hammer time! */
-    private SuspensionPenalty() {
-    }
 
     /**
      * The minimum delay between subsequent attempts to verify a key
      * in milliseconds.
      */
     public static final int MIN_KEY_RETRY_DELAY = 3 * 1000;
+
+    /* Can't touch this - hammer time! */
+    private SuspensionPenalty() { }
 
     /**
      * Call this method in a key verification loop in order to enforce a
@@ -40,19 +38,15 @@ public class SuspensionPenalty {
      *         call.
      * @return The new try time.
      */
-    @SuppressWarnings("SleepWhileInLoop")
     public static long enforce(final long last) {
-        long delay;
-        boolean interrupted = false;
-        while ((delay = System.currentTimeMillis() - last) < MIN_KEY_RETRY_DELAY) {
-            try {
-                Thread.sleep(MIN_KEY_RETRY_DELAY - delay);
-            } catch (InterruptedException ex) {
-                interrupted = true;
-            }
+        final long start = System.currentTimeMillis();
+        final long elapsed = start - last;
+        final long delay = MIN_KEY_RETRY_DELAY - elapsed;
+        if (0 < delay) {
+            Threads.pause(delay);
+            return start + delay; // ~ System.currentTimeMillis()
+        } else {
+            return start; // ~ System.currentTimeMillis()
         }
-        if (interrupted)
-            Thread.currentThread().interrupt(); // restore
-        return last + delay; // approximately System.currentTimeMillis()
     }
 }
