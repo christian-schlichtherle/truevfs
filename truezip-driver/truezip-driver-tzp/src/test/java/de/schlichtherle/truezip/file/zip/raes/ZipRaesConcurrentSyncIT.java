40143c4181761dace26ed76f260366d455eb40a7
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-03 01:18:31 +0100

    RCB

--- truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/file/zip/raes/ZipRaesConcurrentSyncIT.java
+++ truezip-driver/truezip-driver-tzp/src/test/java/de/schlichtherle/truezip/file/zip/raes/ZipRaesConcurrentSyncIT.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.file.zip.raes;
 
@@ -12,8 +8,7 @@ import de.schlichtherle.truezip.file.ConcurrentSyncTestSuite;
 import de.schlichtherle.truezip.fs.archive.zip.raes.TestZipRaesDriver;
 
 /**
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 public final class ZipRaesConcurrentSyncIT extends ConcurrentSyncTestSuite<TestZipRaesDriver> {
 
