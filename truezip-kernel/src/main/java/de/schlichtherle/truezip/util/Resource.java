3cae59bd19f921c6d0f6d35e28fec1066089a028
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-03 04:52:49 +0100

    Cleanup.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/util/Resource.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/util/Resource.java
@@ -1,15 +1,10 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.util;
 
 import edu.umd.cs.findbugs.annotations.CleanupObligation;
-import edu.umd.cs.findbugs.annotations.CreatesObligation;
 import edu.umd.cs.findbugs.annotations.DischargesObligation;
 import java.io.Closeable;
 import java.io.IOException;
@@ -17,21 +12,16 @@ import java.io.IOException;
 /**
  * An abstract closeable resource.
  * 
- * @param   <X> The exception type which may get thrown by {@link #close()}.
- *          If this is an {@link IOException}, then the subclass can implement
- *          the {@link Closeable} interface, too.
- * @since   TrueZIP 7.5
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @param  <X> The exception type which may get thrown by {@link #close()}.
+ *         If this is an {@link IOException}, then the subclass can implement
+ *         the {@link Closeable} interface, too.
+ * @since  TrueZIP 7.5
+ * @author Christian Schlichtherle
  */
 @CleanupObligation
 public abstract class Resource<X extends Exception>
 /*implements AutoCloseable*/ {
     private boolean closed;
-
-    @CreatesObligation
-    protected Resource() {
-    }
 
     /**
      * Closes this resource.
