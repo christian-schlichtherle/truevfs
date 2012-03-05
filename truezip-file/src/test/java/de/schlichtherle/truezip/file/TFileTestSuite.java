bde79377fff1031788e8457dc5ddc976fc4be18d
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-05 01:34:35 +0100

    RCB

--- truezip-file/src/test/java/de/schlichtherle/truezip/file/TFileTestSuite.java
+++ truezip-file/src/test/java/de/schlichtherle/truezip/file/TFileTestSuite.java
@@ -1379,7 +1379,7 @@ extends ConfiguredClientTestBase<D> {
     }
     
     private void assertMultithreadedMultipleArchivesSingleEntryWriting(
-            final boolean updateIndividually)
+            final boolean syncIndividually)
     throws Exception {
         assertTrue(TFile.isLenient());
 
@@ -1398,11 +1398,11 @@ extends ConfiguredClientTestBase<D> {
                     try {
                         createTestFile(entry);
                         try {
-                            if (updateIndividually)
+                            if (syncIndividually)
                                 TFile.umount(archive);
                             else
                                 TFile.sync(SYNC); // DON'T clear cache!
-                        } catch (FsSyncException ex) {
+                        } catch (FsSyncWarningException ex) {
                             if (!(ex.getCause() instanceof FileBusyException))
                                 throw ex;
                             // Some other thread is busy updating an archive.
@@ -1410,11 +1410,11 @@ extends ConfiguredClientTestBase<D> {
                             // could never happen.
                             // Otherwise, silently ignore this exception and
                             // accept that the archive may not have been
-                            // updated to disk.
+                            // synced to disk.
                             // Note that no data is lost, this exception just
                             // signals that the corresponding archive hasn't
                             // been updated - a future call may still succeed.
-                            if (updateIndividually)
+                            if (syncIndividually)
                                 throw new AssertionError(ex);
                         }
                     } finally {
