69c5508af9b59be5496802d53416a4410facb28f
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-05 03:36:28 +0100

    Added more driver tests.

---
+++ truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/fs/archive/zip/CheckedOdfDriverTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package de.schlichtherle.truezip.fs.archive.zip;
+
+import de.schlichtherle.truezip.fs.archive.FsCharsetArchiveDriverTestSuite;
+
+/**
+ * @author Christian Schlichtherle
+ */
+public final class CheckedOdfDriverTest
+extends FsCharsetArchiveDriverTestSuite<ZipDriverEntry, CheckedOdfDriver> {
+
+    @Override
+    protected CheckedOdfDriver newArchiveDriver() {
+        return new CheckedOdfDriver(getTestConfig().getIOPoolProvider());
+    }
+
+    @Override
+    protected String getUnencodableName() {
+        return null;
+    }
+}
