e2d9e334458abe87dc872b3de3c24542af0031f3
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-05 03:36:43 +0100

    Reviewed.

--- truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/fs/archive/zip/ZipArchiveEntryTest.java
+++ truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/fs/archive/zip/ZipArchiveEntryTest.java
@@ -1,21 +1,16 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive.zip;
 
 import de.schlichtherle.truezip.fs.archive.FsArchiveEntry;
 import de.schlichtherle.truezip.zip.ZipEntry;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * @author Christian Schlichtherle
- * @version $Id$
  */
 public class ZipArchiveEntryTest {
 
