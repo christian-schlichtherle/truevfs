66be27daa484ec87668b944f8303498e3a6fee5a
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-05 22:00:39 +0100

    Reviewed.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/io/OutputBusyException.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/io/OutputBusyException.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.io;
 
@@ -18,9 +14,8 @@ import javax.annotation.concurrent.ThreadSafe;
  * operation successfully as soon as the entry or its container is not busy
  * anymore and unless no other exceptional condition applies.
  *
- * @see     InputBusyException
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @see    InputBusyException
+ * @author Christian Schlichtherle
  */
 @ThreadSafe
 public class OutputBusyException extends FileBusyException {
