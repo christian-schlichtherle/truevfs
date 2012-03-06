5c95171fa490213579d9075fbdf94548aa2712ca
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-06 03:06:56 +0100

    Added FindBugs annotations.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/archive/FsReadOnlyArchiveFileSystem.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/archive/FsReadOnlyArchiveFileSystem.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs.archive;
 
@@ -16,6 +12,8 @@ import de.schlichtherle.truezip.fs.FsEntryName;
 import de.schlichtherle.truezip.fs.FsOutputOption;
 import de.schlichtherle.truezip.util.BitField;
 import java.util.Map;
+import javax.annotation.CheckForNull;
+import javax.annotation.WillNotClose;
 import javax.annotation.concurrent.NotThreadSafe;
 
 /**
@@ -23,17 +21,16 @@ import javax.annotation.concurrent.NotThreadSafe;
  * <p>
  * All modifying methods throw a {@link FsReadOnlyArchiveFileSystemException}.
  *
- * @param   <E> The type of the archive entries.
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @param  <E> The type of the archive entries.
+ * @author Christian Schlichtherle
  */
 @NotThreadSafe
 final class FsReadOnlyArchiveFileSystem<E extends FsArchiveEntry>
 extends FsArchiveFileSystem<E> {
 
-    FsReadOnlyArchiveFileSystem(final EntryContainer<E> archive,
+    FsReadOnlyArchiveFileSystem(final @WillNotClose EntryContainer<E> archive,
                                 final FsArchiveDriver<E> driver,
-                                final Entry rootTemplate) {
+                                final @CheckForNull Entry rootTemplate) {
         super(driver, archive, rootTemplate);
     }
 
