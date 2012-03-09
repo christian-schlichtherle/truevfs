cb4557c45afa0b92a834891713f35fe3acb19128
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-09 04:51:51 +0100

    RCB

--- truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/FsArchiveDriverTestSuite.java
+++ truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/FsArchiveDriverTestSuite.java
@@ -620,13 +620,6 @@ extends FsArchiveDriverTestBase<D> {
             super(out);
         }
 
-        // Simulate IOException from FsLockController.LockOutputStream.flush()
-        /*@Override
-        public void flush() throws IOException {
-            checkAllExceptions(this);
-            delegate.flush();
-        }*/
-
         @Override
         public void close() throws IOException {
             checkAllExceptions(this);
