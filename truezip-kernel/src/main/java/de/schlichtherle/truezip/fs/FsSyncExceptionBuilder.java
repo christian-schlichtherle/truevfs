bee922ddc52efce19880cebc4b6ea95b3b707af9
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-12 05:06:05 +0100

    Updated documentation and added tests.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsSyncExceptionBuilder.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/fs/FsSyncExceptionBuilder.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.fs;
 
@@ -20,8 +16,7 @@ import javax.annotation.concurrent.NotThreadSafe;
  * When the assembly is thrown or returned later, it is sorted by
  * {@link SequentialIOException#sortPriority() priority}.
  *
- * @author  Christian Schlichtherle
- * @version $Id$
+ * @author Christian Schlichtherle
  */
 @NotThreadSafe
 public final class FsSyncExceptionBuilder
