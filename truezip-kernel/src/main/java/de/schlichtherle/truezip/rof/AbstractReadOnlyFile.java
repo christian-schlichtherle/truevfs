dbc9c19ecec3ad283bb645ae1b3a60fd00c26796
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-17 11:04:57 +0100

    RCB

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/rof/AbstractReadOnlyFile.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/rof/AbstractReadOnlyFile.java
@@ -1,10 +1,6 @@
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (C) 2004-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package de.schlichtherle.truezip.rof;
 
@@ -37,9 +33,9 @@ public abstract class AbstractReadOnlyFile implements ReadOnlyFile {
         int total = 0;
         do {
             final int read = read(buf, off + total, len - total);
-            if (read < 0)
+            if (0 > read)
                 throw new EOFException();
             total += read;
-        } while (total < len);
+        } while (len > total);
     }
 }
