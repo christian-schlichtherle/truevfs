8f0a4ab701233811dbbb2d6e028ba578927c4b06
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-18 14:57:23 +0100

    Copyright.

--- truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/file/zip/OdfConcurrentSyncIT.java
+++ truezip-driver/truezip-driver-zip/src/test/java/de/schlichtherle/truezip/file/zip/OdfConcurrentSyncIT.java
@@ -1,7 +1,7 @@
 package de.schlichtherle.truezip.file.zip;
 
 /*
- * Copyright 2004-2012 Schlichtherle IT Services
+ * Copyright (C) 2005-2012 Schlichtherle IT Services
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
