f196cd35f74a279d699488e0e2fc6324709e86a0
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-23 07:50:09 +0100

    Made the lock accessible to sub classes.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/io/LockOutputStream.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/io/LockOutputStream.java
@@ -25,7 +25,7 @@ import javax.annotation.concurrent.ThreadSafe;
 @ThreadSafe
 public class LockOutputStream extends DecoratingOutputStream {
 
-    /** The object to synchronize on. */
+    /** The lock on which this object synchronizes. */
     protected final Lock lock;
 
     /**
