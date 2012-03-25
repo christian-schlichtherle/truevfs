3d79098eed7efcb8174ae3231d64fe13416491ce
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-03-25 16:13:05 +0200

    Updated documentation.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/sl/package-info.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/sl/package-info.java
@@ -3,16 +3,10 @@
  * All rights reserved. Use is subject to license terms.
  */
 /**
- * Provides singletons to locate service providers for
- * <ul>
- * <li>file system driver maps,</li>
- * <li>file system managers and</li>
- * <li>I/O buffer pools</li>
- * </ul>
- * on the class path.
- * <p>
- * Note that the module TrueZIP Kernel has no dependency on this package
- * - using it is entirely optional.
+ * Provides singletons to locate various service providers on the class path.
+ * Note that this module has absolutely no dependency on this package!
+ * It's up to the modules in the access tier to inject the service locators
+ * into this module if they want to.
  */
 @edu.umd.cs.findbugs.annotations.DefaultAnnotation(edu.umd.cs.findbugs.annotations.NonNull.class)
 package de.schlichtherle.truezip.sl;
