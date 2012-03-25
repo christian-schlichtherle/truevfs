7543145eb0c75a64ec934c338130ca9d279c830a
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-25 13:33:25 +0200

    Renamed packages in TrueZIP Driver FILE.

---
+++ truezip-driver/truezip-driver-file/src/test/java/de/truezip/driver/file/nio/FileDriverServiceTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package de.truezip.driver.file.nio;
+
+import de.schlichtherle.truezip.fs.addr.FsScheme;
+import de.truezip.driver.file.nio.FileDriverService;
+import de.schlichtherle.truezip.fs.spi.FsDriverService;
+import de.schlichtherle.truezip.util.SuffixSet;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author  Christian Schlichtherle
+ */
+public final class FileDriverServiceTest {
+
+    public static final String DRIVER_LIST = "file";
+
+    private FsDriverService instance;
+
+    @Before
+    public void setUp() {
+        instance = new FileDriverService();
+    }
+
+    @Test
+    public void testGet() {
+        for (String scheme : new SuffixSet(DRIVER_LIST))
+            assertThat(instance.get().get(FsScheme.create(scheme)), notNullValue());
+    }
+
+    @Test
+    public void testImmutability() {
+        try {
+            instance.get().remove(FsScheme.create("file"));
+            fail("put");
+        } catch (UnsupportedOperationException ex) {
+        }
+    }
+}
