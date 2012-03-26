89eef05de42e717569e9f554145e6eab049369c0
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-26 10:23:56 +0200

    Made the test setup comparable to ZIP.RAES.

--- truezip-driver/truezip-driver-zip/src/test/java/de/truezip/driver/zip/file/WinZipAesFileIT.java
+++ truezip-driver/truezip-driver-zip/src/test/java/de/truezip/driver/zip/file/WinZipAesFileIT.java
@@ -5,12 +5,24 @@
 package de.truezip.driver.zip.file;
 
 import de.truezip.driver.zip.TestWinZipAesDriver;
+import de.truezip.file.TFile;
 import de.truezip.file.TFileITSuite;
+import de.truezip.kernel.key.impl.MockView;
+import static de.truezip.kernel.key.impl.MockView.Action.CANCEL;
+import static de.truezip.kernel.key.impl.MockView.Action.ENTER;
+import de.truezip.kernel.key.param.AesPbeParameters;
+import java.io.IOException;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  * @author  Christian Schlichtherle
  */
 public final class WinZipAesFileIT extends TFileITSuite<TestWinZipAesDriver> {
+
+    private MockView<AesPbeParameters> view;
+
     @Override
     protected String getSuffixList() {
         return "zip";
@@ -20,6 +32,56 @@ public final class WinZipAesFileIT extends TFileITSuite<TestWinZipAesDriver> {
     protected TestWinZipAesDriver newArchiveDriver() {
         final TestWinZipAesDriver driver = new TestWinZipAesDriver(
                 getTestConfig().getIOPoolProvider());
+        view = driver.getView();
         return driver;
+    }
+
+    @Test
+    public void testCancelling() throws IOException {
+        view.setAction(CANCEL);
+
+        final TFile archive = getArchive();
+        assertFalse(archive.toNonArchiveFile().exists());
+
+        final TFile entry1 = new TFile(archive, "entry1");
+        assertTrue(entry1.mkdirs());
+        entry1.rm();
+        assertTrue(entry1.createNewFile());
+        entry1.rm();
+
+        final TFile entry2 = new TFile(archive, "entry2");
+        assertTrue(entry2.mkdirs());
+        entry2.rm();
+        assertTrue(entry2.createNewFile());
+        entry2.rm();
+    }
+
+    @Test
+    public void testFileStatus() throws IOException {
+        final TFile archive = getArchive();
+        final TFile inner = new TFile(archive, "inner" + getSuffix());
+
+        assertTrue(archive.mkdir());
+        assertTrue(inner.mkdir());
+
+        umount();
+        view.setAction(CANCEL);
+        assertTrue(archive.exists());
+        assertTrue(archive.isDirectory());
+        assertFalse(archive.isFile());
+
+        umount();
+        view.setAction(ENTER);
+        assertTrue(archive.exists());
+        assertTrue(archive.isDirectory());
+        assertFalse(archive.isFile());
+
+        view.setAction(CANCEL);
+        assertTrue(inner.exists());
+        assertTrue(inner.isDirectory());
+        assertFalse(inner.isFile());
+
+        umount();
+        archive.rm_r();
     }
 }
