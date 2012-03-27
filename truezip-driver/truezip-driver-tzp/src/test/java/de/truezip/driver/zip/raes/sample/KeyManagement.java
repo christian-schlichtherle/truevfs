46fc3e460838eac62a0b907c89b41aa77c4fa79d
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-03-27 17:29:08 +0200

    Fixed a few bugs.

--- truezip-driver/truezip-driver-tzp/src/test/java/de/truezip/driver/zip/raes/sample/KeyManagement.java
+++ truezip-driver/truezip-driver-tzp/src/test/java/de/truezip/driver/zip/raes/sample/KeyManagement.java
@@ -4,9 +4,6 @@
  */
 package de.truezip.driver.zip.raes.sample;
 
-import de.schlichtherle.truezip.key.PromptingKeyProviderController;
-import de.schlichtherle.truezip.key.PromptingKeyProviderView;
-import de.schlichtherle.truezip.key.spi.PromptingKeyManagerService;
 import de.truezip.driver.zip.raes.SafeZipRaesDriver;
 import de.truezip.driver.zip.raes.crypto.RaesKeyException;
 import de.truezip.driver.zip.raes.crypto.RaesParameters;
@@ -17,10 +14,7 @@ import de.truezip.kernel.fs.FsController;
 import de.truezip.kernel.fs.FsDriverProvider;
 import de.truezip.kernel.fs.FsModel;
 import de.truezip.kernel.sl.IOPoolLocator;
-import de.truezip.key.KeyManagerProvider;
-import de.truezip.key.UnknownKeyException;
 import de.truezip.key.param.AesKeyStrength;
-import de.truezip.key.param.AesPbeParameters;
 
 /**
  * Provides static utility methods to set passwords for RAES encrypted ZIP
@@ -29,13 +23,12 @@ import de.truezip.key.param.AesPbeParameters;
  * programmatically instead of prompting the user for a key by means of the
  * default Swing or Console based user interfaces.
  *
- * @author  Christian Schlichtherle
+ * @author Christian Schlichtherle
  */
 public final class KeyManagement {
 
-    /** You cannot instantiate this class. */
-    private KeyManagement() {
-    }
+    /* Can't touch this - hammer time! */
+    private KeyManagement() { }
 
     static void install(TArchiveDetector detector) {
 // START SNIPPET: install
@@ -43,7 +36,7 @@ public final class KeyManagement {
 // END SNIPPET: install
     }
 
-// START SNIPPET: newArchiveDetector1
+// START SNIPPET: newArchiveDetector
     /**
      * Returns a new archive detector which uses the given password for all
      * RAES encrypted ZIP files with the given list of suffixes.
@@ -62,7 +55,7 @@ public final class KeyManagement {
      * @return A new archive detector which uses the given password for all
      *         RAES encrypted ZIP files with the given list of suffixes.
      */
-    public static TArchiveDetector newArchiveDetector1(
+    public static TArchiveDetector newArchiveDetector(
             FsDriverProvider delegate,
             String suffixes,
             char[] password) {
@@ -93,10 +86,11 @@ public final class KeyManagement {
         protected FsController<?> decorate(FsController<?> controller) {
             // This is a minor improvement: The default implementation decorates
             // the default file system controller chain with a package private
-            // file system controller which keeps track of the encryption keys.
+            // file system controller which uses the key manager to keep track
+            // of the encryption keys.
             // Because we are not using the key manager, we don't need this
-            // special purpose file system controller and can return the given
-            // file system controller chain instead.
+            // special purpose file system controller and can simply return the
+            // given file system controller chain instead.
             return controller;
         }
     } // CustomZipRaesDriver
@@ -136,91 +130,5 @@ public final class KeyManagement {
             assert AesKeyStrength.BITS_128 == keyStrength;
         }
     } // CustomRaesParameters
-// END SNIPPET: newArchiveDetector1
-
-// START SNIPPET: newArchiveDetector2
-    /**
-     * Returns a new archive detector which uses the given password for all
-     * RAES encrypted ZIP files with the given list of suffixes.
-     * <p>
-     * When used for encryption, the AES key strength will be set to 128 bits.
-     * <p>
-     * A protective copy of the given password char array is made.
-     * It's recommended to overwrite the parameter array with any non-password
-     * data after calling this method.
-     *
-     * @param  delegate the file system driver provider to decorate.
-     * @param  suffixes A list of file name suffixes which shall identify
-     *         prospective archive files.
-     *         This must not be {@code null} and must not be empty.
-     * @param  password the password char array to be copied for internal use.
-     * @return A new archive detector which uses the given password for all
-     *         RAES encrypted ZIP files with the given list of suffixes.
-     */
-    public static TArchiveDetector newArchiveDetector2(
-            FsDriverProvider delegate,
-            String suffixes,
-            char[] password) {
-        return new TArchiveDetector(delegate,
-                    suffixes, new CustomZipRaesDriver2(password));
-    }
-
-    private static final class CustomZipRaesDriver2 extends SafeZipRaesDriver {
-        final KeyManagerProvider provider;
-        
-        CustomZipRaesDriver2(char[] password) {
-            super(IOPoolLocator.SINGLETON);
-            this.provider = new PromptingKeyManagerService(
-                    new CustomView(password));
-        }
-
-        @Override
-        protected KeyManagerProvider getKeyManagerProvider() {
-            return provider;
-        }
-    } // CustomZipRaesDriver2
-
-    private static final class CustomView
-    implements PromptingKeyProviderView<AesPbeParameters> {
-        final char[] password;
-        
-        CustomView(char[] password) {
-            this.password = password.clone();
-        }
-        
-        /**
-         * You need to create a new key because the key manager may eventually
-         * reset it when the archive file gets moved or deleted.
-         */
-        private AesPbeParameters newKey() {
-            AesPbeParameters param = new AesPbeParameters();
-            param.setPassword(password);
-            param.setKeyStrength(AesKeyStrength.BITS_128);
-            return param;
-        }
-        
-        @Override
-        public void promptWriteKey(PromptingKeyProviderController<AesPbeParameters> controller)
-        throws UnknownKeyException {
-            // You might as well call controller.getResource() here in order to
-            // programmatically set the parameters for individual resource URIs.
-            // Note that this would typically return the hierarchical URI of
-            // the archive file unless ZipDriver.mountPointUri(FsModel) would
-            // have been overridden.
-            controller.setKey(newKey());
-        }
-        
-        @Override
-        public void promptReadKey(  PromptingKeyProviderController<AesPbeParameters> controller,
-                                    boolean invalid)
-        throws UnknownKeyException {
-            // You might as well call controller.getResource() here in order to
-            // programmatically set the parameters for individual resource URIs.
-            // Note that this would typically return the hierarchical URI of
-            // the archive file unless ZipDriver.mountPointUri(FsModel) would
-            // have been overridden.
-            controller.setKey(newKey());
-        }
-    } // CustomView
-// END SNIPPET: newArchiveDetector2
+// END SNIPPET: newArchiveDetector
 }
