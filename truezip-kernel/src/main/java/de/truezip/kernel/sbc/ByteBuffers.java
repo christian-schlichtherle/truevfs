0f4fdcd8d293d5e741b70aca8064a59b33ea76e7
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-04-04 08:17:45 +0200

    Merged packages.

--- truezip-kernel/src/main/java/de/truezip/kernel/sbc/ByteBuffers.java
+++ truezip-kernel/src/main/java/de/truezip/kernel/sbc/ByteBuffers.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2005-2012 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-package de.truezip.kernel.bb;
+package de.truezip.kernel.sbc;
 
 import java.nio.ByteBuffer;
 
@@ -11,7 +11,7 @@ import java.nio.ByteBuffer;
  * 
  * @author Christian Schlichtherle
  */
-public final class ByteBuffers {
+final class ByteBuffers {
 
     /** Can't touch this - hammer time! */
     private ByteBuffers() { }
