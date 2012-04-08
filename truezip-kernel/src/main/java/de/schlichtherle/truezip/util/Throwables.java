fe3dd2a56f68ecf0ffe84853aa3dd494332c2495
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-04-08 16:26:22 +0200

    Improved exception handling.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/util/Throwables.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/util/Throwables.java
@@ -19,17 +19,15 @@ public class Throwables {
     private Throwables() { }
 
     /**
-     * Wraps the given throwable in a new instance of the same class.
+     * Wraps the given throwable in a new instance of the same class if
+     * possible.
      * 
      * @param  <T> the compile time type of the throwable to return.
      * @param  t the throwable to wrap in a new instance of the same class.
      * @return a new instance of the same class as the throwable {@code t}
      *         with its {@linkplain Throwable#getCause() cause} initialized to
-     *         {@code t}.
-     * @throws IllegalArgumentException If the class of the throwable {@code t}
-     *         does not have a public constructor with a single string
-     *         parameter or the cause of the new instance cannot get
-     *         {@linkplain Throwable#initCause initialized} to {@code t}.
+     *         {@code t} or simply {@code t} if the wrapping fails due to
+     *         another exception.
      */
     @SuppressWarnings("unchecked")
     public static <T extends Throwable> T wrap(final T t) {
@@ -38,11 +36,9 @@ public class Throwables {
                         .getConstructor(String.class)
                         .newInstance(t.toString())
                         .initCause(t);
-        } catch (final RuntimeException ex) {
-            // E.g. null == t || ex instanceof SecurityException
-            throw ex;
         } catch (final Exception ex) {
-            throw new IllegalArgumentException(t.toString(), ex);
+            //t.addSuppressed(ex);
+            return t;
         }
     }
 
