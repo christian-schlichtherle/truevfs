873c63cd99184a9342d39dc95dfc88e98319970d
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-04-09 13:04:42 +0200

    Fixed documentation.

--- truezip-driver/truezip-driver-zip/src/main/java/de/truezip/driver/zip/CheckedOdfDriver.java
+++ truezip-driver/truezip-driver-zip/src/main/java/de/truezip/driver/zip/CheckedOdfDriver.java
@@ -8,22 +8,22 @@ import de.truezip.kernel.cio.IOPoolProvider;
 import javax.annotation.concurrent.Immutable;
 
 /**
- * An archive driver for ODF files which checks the CRC-32 value for all ZIP
+ * An archive driver for ODF files which checks the CRC32 value for all ZIP
  * entries in input archives.
- * The additional CRC-32 computation makes this class slower than its super
+ * The additional CRC32 computation makes this class slower than its super
  * class.
  * <p>
- * If there is a mismatch of the CRC-32 values for a ZIP entry in an input
+ * If there is a mismatch of the CRC32 values for a ZIP entry in an input
  * archive, the {@link java.io.InputStream#close} method of the corresponding
  * stream for the archive entry will throw a
- * {@link de.truezip.driver.zip.io.CRC32Exception}.
+ * {@link de.truezip.driver.zip.io.Crc32Exception}.
  * Other than this, the archive entry will be processed normally.
- * So if just the CRC-32 value for the entry in the archive file has been
+ * So if just the CRC32 value for the entry in the archive file has been
  * modified, you can still read its entire contents.
  * <p>
  * Subclasses must be thread-safe and should be immutable!
  * 
- * @author  Christian Schlichtherle
+ * @author Christian Schlichtherle
  */
 @Immutable
 public class CheckedOdfDriver extends OdfDriver {
