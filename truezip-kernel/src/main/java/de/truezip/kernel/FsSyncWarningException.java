2936ce5d4b955c217eefe61c422fe3494dd58e98
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-04-10 17:37:51 +0200

    Generalized exception.

--- truezip-kernel/src/main/java/de/truezip/kernel/FsSyncWarningException.java
+++ truezip-kernel/src/main/java/de/truezip/kernel/FsSyncWarningException.java
@@ -4,23 +4,21 @@
  */
 package de.truezip.kernel;
 
-import java.io.IOException;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Indicates an exceptional condition when synchronizing the changes in a
  * federated file system to its parent file system.
  * An exception of this class implies that no or only insignificant parts
- * of the data of the federated file system has been lost.
+ * of the data in the federated file system has been lost.
  * 
  * @author Christian Schlichtherle
  */
 @ThreadSafe
 public class FsSyncWarningException extends FsSyncException {
-
     private static final long serialVersionUID = 2302357394858347366L;
 
-    public FsSyncWarningException(FsModel model, IOException cause) {
+    public FsSyncWarningException(FsModel model, Throwable cause) {
         super(model, cause);
     }
 
