116a86852ea15ab7ae99730cd29fbda61a501e17
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-04-11 13:32:51 +0200

    Improved exception handling.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/kernel/SyncDecoratingController.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/kernel/SyncDecoratingController.java
@@ -35,17 +35,11 @@ extends FsDecoratingController<M, C> {
     final void
     sync(final NeedsSyncException trigger)
     throws IOException {
-        final FsSyncWarningException fuse
-                = new FsSyncWarningException(getModel(), trigger);
-        final FsSyncExceptionBuilder ied = new FsSyncExceptionBuilder();
         try {
-            ied.warn(fuse);     // charge fuse
-            sync(SYNC, ied);    // charge load
-            ied.check();        // pull trigger
-            throw new AssertionError("Expected to throw an instance of the " + FsSyncException.class);
-        } catch (final FsSyncWarningException damage) {
-            if (damage != fuse) // check for dud
-                throw damage;
+            sync(SYNC);
+        } catch (final IOException ex) {
+            ex.addSuppressed(trigger);
+            throw ex;
         }
     }
 }
