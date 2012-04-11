8e5afa4cf7b0f9aa58c3ffbf307907a2461cdc51
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-04-11 16:48:23 +0200

    Dealing with Throwables.

--- truezip-kernel/src/main/java/de/schlichtherle/truezip/kernel/FailSafeManager.java
+++ truezip-kernel/src/main/java/de/schlichtherle/truezip/kernel/FailSafeManager.java
@@ -5,12 +5,8 @@
 package de.schlichtherle.truezip.kernel;
 
 import de.truezip.kernel.*;
-import de.truezip.kernel.FsMountPoint;
-import de.truezip.kernel.FsSyncOption;
-import de.truezip.kernel.FsSyncOptions;
 import de.truezip.kernel.util.BitField;
 import de.truezip.kernel.util.ExceptionHandler;
-import java.io.IOException;
 import javax.annotation.CheckForNull;
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -114,7 +110,7 @@ final class FailSafeManager extends FsDecoratingManager<FsManager> {
         public void run() {
             try {
                 manager.sync(FsSyncOptions.UMOUNT);
-            } catch (final IOException ex) {
+            } catch (final Throwable ex) {
                 // Logging doesn't work in a shutdown hook!
                 ex.printStackTrace();
             }
