72ed03943c415efb9bd2adbde3d520abd7550f17
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-04-11 13:12:36 +0200

    Merged packages.

--- truezip-extension/truezip-extension-jmx-jul/src/main/java/de/truezip/extension/jmxjul/CompositeIOPoolService.java
+++ truezip-extension/truezip-extension-jmx-jul/src/main/java/de/truezip/extension/jmxjul/CompositeIOPoolService.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2005-2012 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-package de.truezip.extension.jmxjul.comp;
+package de.truezip.extension.jmxjul;
 
 import de.truezip.driver.file.TempFilePoolService;
 import de.truezip.extension.jmxjul.jmx.JmxDirector;
