457d851be9993fe15795ef41e669846dd141e57c
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-05-04 19:31:12 +0200

    Backed out changeset: 93e573f42b17

--- truezip-kernel/src/main/java/de/truezip/kernel/FsModelController.java
+++ truezip-kernel/src/main/java/de/truezip/kernel/FsModelController.java
@@ -14,7 +14,7 @@ import java.util.Objects;
  * @author Christian Schlichtherle
  */
 public abstract class FsModelController<M extends FsModel>
-extends FsAbstractController<M>  {
+extends FsController<M>  {
 
     private final M model;
 
