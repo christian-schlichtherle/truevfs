ff61ce0102f444898e24fb9552e8a022ba4a17aa
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-05-24 12:25:33 +0200

    Spinned off Tree.

--- truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/TreeContainer.scala
+++ truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/TreeContainer.scala
@@ -16,141 +16,18 @@ import TreeContainer._
  * @param  <E> the type of the entries in this container.
  * @author Christian Schlichtherle
  */
-class TreeContainer[E >: Null <: Entry]
-(splitter: Splitter = new Splitter('/'))
+class TreeContainer[E >: Null <: Entry](splitter: Splitter = new Splitter('/'))
 extends Container[E] {
 
-  private implicit def container = this
+  private[this] val tree = new Tree[E](splitter)
 
-  private[this] val rootNode: Node[E] = new Node(None)
+  def apply(path: String) = tree.apply(path)
 
-  private var _size: Int = _
+  def update(path: String, entry: Option[E]) = tree.update(path, entry)
 
-  def apply(path: String) = node(path) flatMap (_ entry)
+  override def size = tree size
+  
+  override def iterator = collection.JavaConversions.asIterator(tree iterator)
 
-  private def node(path: String): Option[Node[E]] = {
-    if (path isEmpty) {
-      new Some(rootNode)
-    } else {
-      val (parentPath, memberName) = splitter(path)
-      node(parentPath) flatMap (_(memberName))
-    }
-  }
-
-  def update(path: String, entry: Option[E]) {
-    entry match {
-      case Some(e) => require(null ne e); add(path, entry)
-      case _       => remove(path)
-    }
-  }
-
-  def add(path: String, entry: E): Boolean = {
-    require(null ne entry)
-    add(path, Some(entry)).entry isEmpty
-  }
-
-  private def add(path: String, entry: Option[E]): Node[E] = {
-    if (path isEmpty) {
-      entry foreach (_ => rootNode entry = entry)
-      rootNode
-    } else {
-      val (parentPath, memberName) = splitter(path)
-      val parentNode = add(parentPath, None)
-      parentNode(memberName) match {
-        case Some(memberNode) =>
-          entry.foreach(_ => memberNode entry = entry)
-          memberNode
-        case _ =>
-          val memberNode = new Node(entry)
-          parentNode(memberName) = Some(memberNode)
-          memberNode
-      }
-    }
-  }
-
-  def remove(path: String) = {
-    if (path isEmpty) {
-      if (0 != rootNode.size)
-        throw new IllegalStateException("Node not empty!")
-      val rootEntry = rootNode.entry
-      rootNode entry = None
-      rootEntry
-    } else {
-      val (parentPath, memberName) = splitter(path)
-      node(parentPath) flatMap { parentNode =>
-        parentNode(memberName) flatMap { memberNode =>
-          // HC SVNT DRACONES!
-          if (0 != memberNode.size)
-            throw new IllegalStateException("Node not empty!")
-          parentNode(memberName) = None
-          val oldEntry = memberNode.entry
-          memberNode entry = None // update _size conditionally
-          oldEntry
-        }
-      }
-    }
-  } isDefined
-
-  override def size = _size
-
-  override def iterator =
-    collection.JavaConversions.asIterator(rootNode recursiveIterator)
-
-  override def entry(path: String) = apply(path) orNull
+  override def entry(path: String) = tree(path) orNull
 } // TreeContainer
-
-object TreeContainer {
-
-  private final class Node[E >: Null <: Entry]
-  (private[this] var _entry: Option[E])
-  (implicit container: TreeContainer[E])
-  extends collection.Iterable[Node[E]] {
-
-    private[this] var _members = collection.SortedMap[String, Node[E]]()
-
-    if (_entry isDefined)
-      container._size += 1
-
-    def entry = _entry
-
-    def entry_=(entry: Option[E])(implicit container: TreeContainer[E]) {
-      // HC SVNT DRACONES!
-      if (_entry isDefined) {
-        if (entry isEmpty)
-          container._size -= 1
-      } else {
-        if (entry isDefined)
-          container._size += 1
-      }
-      _entry = entry
-    }
-
-    def apply(name: String) = _members get name
-
-    def update(name: String, node: Option[Node[E]]) {
-      node match {
-        case Some(node) => _members += new String(name) -> node // don't share sub-strings!
-        case _          => _members -= name
-      }
-    }
-
-    override def iterator = _members.valuesIterator filter (_.entry isDefined)
-
-    def recursiveIterator: Iterator[E] =
-      _entry.iterator ++ _members.valuesIterator.flatMap(_ recursiveIterator)
-  } // Node
-} // TreeContainer
-
-/*class AnonymousEntry extends Entry {
-  final override def getName = null
-}*/
-
-/*class TreeEntry[E <: AnonymousEntry] private(
-  node: TreeContainer.Node[E],
-  parent: Option[TreeContainer.Node[E]])
-extends DecoratingEntry(node.entry.get) {
-
-  override getName = {
-    
-  }
-}*/
