c4aed9227f13b1e19144494e465e4b4bdbb46bed
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-05-31 00:14:19 +0200

    Renamed.

--- truevfs-kernel/src/test/scala/net/truevfs/kernel/util/FileSystem.scala
+++ truevfs-kernel/src/test/scala/net/truevfs/kernel/util/FileSystem.scala
@@ -5,19 +5,23 @@
 package net.truevfs.kernel.util
 
 import collection._
-import PathMap._
+import FileSystem._
 
 /**
- * A mutable map with decomposable keys.
- * The standard use case is for mapping path name strings which can get
- * decomposed into segments by splitting them with a separator character such
- * as {@code '/'}.
- * Using this map will then help to save some heap space if the path names
- * address deeply nested directory structures where many segments can get
- * shared between mapped values <em>and</em> no external references to the path
- * names are held!
+ * A (virtual) file system is a mutable map from decomposable keys, named
+ * <em>paths</em>, to arbitrary values, named <em>entries</em>.
+ * The standard use case for this class is for mapping path name strings which
+ * can get decomposed into segments by splitting them with a separator
+ * character such as {@code '/'}.
+ * However, this class works with any generic decomposable key type for which
+ * a {@link FileSystem.Composition} and an {@link Ordering} exist.
  * <p>
- * This class supports both {@code null} keys and values.
+ * Using this class helps to save some heap space if the paths address deeply
+ * nested directory trees where many path segments can get shared between
+ * mapped entries &mdash; provided that no other references to the paths are
+ * held!
+ * <p>
+ * This class supports both {@code null} paths and entries.
  * <p>
  * By default, this class uses {@link scala.collection.immutable.SortedMap}s
  * with an implicit ordering passed to its constructor for storing its values.
@@ -27,19 +31,21 @@ import PathMap._
  * <p>
  * This class is <em>not</em> thread-safe!
  * 
- * @param  <V> the type of the values in this map.
+ * @param  <K> the type of the paths (keys) in this (virtual) file system (map).
+ * @param  <V> the type of the entries (values) in this (virtual) file system
+ *         (map).
  * @author Christian Schlichtherle
  */
-class PathMap[K >: Null <: AnyRef, V] protected
+class FileSystem[K >: Null <: AnyRef, V] protected
 (implicit val composition: Composition[K], ordering: Ordering[K])
-extends mutable.Map[K, V] with mutable.MapLike[K, V, PathMap[K, V]] {
+extends mutable.Map[K, V] with mutable.MapLike[K, V, FileSystem[K, V]] {
 
   private[this] var _root: Node[K, V] = _
   private var _size: Int = _
 
   reset()
 
-  implicit private def self = this
+  private implicit def self = this
 
   private def reset() { _root = new Root; _size = 0}
 
@@ -50,11 +56,11 @@ extends mutable.Map[K, V] with mutable.MapLike[K, V, PathMap[K, V]] {
   protected def newDirectory[V]: mutable.Map[K, V] =
     new mutable.ImmutableMapAdaptor(immutable.SortedMap.empty)
 
-  override def empty = new PathMap[K, V]
+  override def empty = new FileSystem[K, V]
 
   override def iterator = _root recursiveEntriesIterator (None)
 
-  override def get(path: K) = node(path) flatMap (_ value)
+  override def get(path: K) = node(path) flatMap (_ entry)
 
   def node(path: K): Option[Node[K, V]] = node(Option(path))
 
@@ -63,7 +69,7 @@ extends mutable.Map[K, V] with mutable.MapLike[K, V, PathMap[K, V]] {
       case Some(path) =>
         path match {
           case composition(parent, segment) =>
-            node(parent) flatMap (_.get(segment))
+            node(parent) flatMap (_ get(segment))
         }
       case None =>
         Some(_root)
@@ -74,112 +80,108 @@ extends mutable.Map[K, V] with mutable.MapLike[K, V, PathMap[K, V]] {
 
   private def list(path: Option[K]) = node(path) map (_ list path)
 
-  override def +=(entry: (K, V)) = {
-    add(entry._1, entry._2)
-    this
-  }
+  override def +=(kv: (K, V)) = { link(kv._1, kv._2); this }
 
-  def add(path: K, value: V): Node[K, V] = add(Option(path), Some(value))
+  def link(path: K, entry: V): Node[K, V] = link(Option(path), Some(entry))
 
-  private def add(path: Option[K], value: Option[V]): Node[K, V] = {
+  private def link(path: Option[K], entry: Option[V]): Node[K, V] = {
     path match {
       case Some(path) =>
         path match {
           case composition(parent, segment) =>
-            add(parent, None) add (segment, value)
+            link(parent, None) add (segment, entry)
         }
       case None =>
-        if (value isDefined) _root value = value
+        if (entry isDefined) _root entry = entry
         _root
     }
   }
 
-  override def -=(path: K) = {
-    remove(Option(path))
-    this
-  }
+  override def -=(path: K) = { unlink(path); this }
 
-  private def remove(path: Option[K]) {
+  def unlink(path: K): Unit = unlink(Option(path))
+
+  private def unlink(path: Option[K]) {
     path match {
       case Some(path) =>
         path match {
           case composition(parent, segment) =>
             node(parent) foreach { node =>
               node remove segment
-              if (node isEmpty) remove(parent)
+              if (node isEmpty) unlink(parent)
             }
         }
       case None =>
-        _root value = None
+        _root entry = None
     }
   }
-} // PathMap
+} // FileSystem
 
-object PathMap {
+object FileSystem {
 
   def apply[K >: Null <: AnyRef : Ordering, V](composition: Composition[K]) =
-    new PathMap[K, V]()(composition, implicitly[Ordering[K]])
+    new FileSystem[K, V]()(composition, implicitly[Ordering[K]])
 
-  def apply[V](separator: Char): PathMap[String, V] =
+  def apply[V](separator: Char): FileSystem[String, V] =
     apply(new StringComposition(separator))
 
-  class Node[K >: Null <: AnyRef, V] private[PathMap] (
+  class Node[K >: Null <: AnyRef, V] private[FileSystem] (
     private[this] val parent: Option[(Node[K, V], K)],
-    private[this] var _value: Option[V])
-  (implicit map: PathMap[K, V]) {
+    private[this] var _entry: Option[V])
+  (implicit map: FileSystem[K, V]) {
 
     private[this] val _members = map.newDirectory[Node[K, V]]
 
-    if (_value isDefined) map._size += 1
+    if (_entry isDefined) map._size += 1
 
-    def address: (PathMap[K, V], Option[K]) = {
-      val (node, segment) = parent.get
+    def address: (FileSystem[K, V], Option[K]) = {
+      val (node, segment) = parent get
       val (map, path) = node.address
-      map -> Some(map.composition(path, segment))
+      map -> Some(map composition (path, segment))
     }
 
-    def path = address._2
+    def path = address _2
 
-    def value = _value
+    def entry = _entry
 
-    private[PathMap] def value_=(value: Option[V])
-    (implicit map: PathMap[K, V]) {
+    private[FileSystem] def entry_=(entry: Option[V])
+    (implicit map: FileSystem[K, V]) {
       // HC SVNT DRACONES!
-      if (_value isDefined) {
-        if (value isEmpty)
+      if (_entry isDefined) {
+        if (entry isEmpty)
           map._size -= 1
       } else {
-        if (value isDefined)
+        if (entry isDefined)
           map._size += 1
       }
-      _value = value
+      _entry = entry
     }
 
-    private[PathMap] def isEmpty = _value.isEmpty && 0 == _members.size
+    private[FileSystem] def isEmpty = _entry.isEmpty && 0 == _members.size
 
-    private[PathMap] final def get(segment: K) = _members get segment
+    private[FileSystem] final def get(segment: K) = _members get segment
 
-    private[PathMap] final def add(segment: K, value: Option[V])
-    (implicit map: PathMap[K, V]) = {
+    private[FileSystem] final def add(segment: K, entry: Option[V])
+    (implicit map: FileSystem[K, V]) = {
       _members get segment match {
         case Some(node) =>
-          if (value isDefined) node value = value
+          if (entry isDefined) node entry = entry
           node
         case None =>
-          val node = new Node[K, V](Some(this, segment), value)
+          val node = new Node[K, V](Some(this, segment), entry)
           _members += segment -> node
           node
       }
     }
 
-    private[PathMap] final def remove(segment: K)(implicit map: PathMap[K, V]) {
+    private[FileSystem] final def remove(segment: K)(implicit map: FileSystem[K, V]) {
       _members get segment map { node =>
-        node value = None
+        node entry = None
         if (node isEmpty) _members -= segment
       }
     }
 
-    private[PathMap] final def recursiveEntriesIterator(path: Option[K])
+    private[FileSystem] final def recursiveEntriesIterator(path: Option[K])
     (implicit composition: Composition[K]): Iterator[(K, V)] = {
       entry(path).iterator ++ _members.iterator.flatMap {
         case (segment, node) =>          
@@ -187,23 +189,23 @@ object PathMap {
       }
     }
 
-    private def entry(path: Option[K]) = _value map (path.orNull -> _)
+    private def entry(path: Option[K]) = _entry map (path.orNull -> _)
 
-    private[PathMap] def list(path: Option[K])
+    private[FileSystem] def list(path: Option[K])
     (implicit composition: Composition[K]) = {
       _members.toIterable flatMap {
-        case (segment, node) => node.value map (composition(path, segment) -> _)
+        case (segment, node) => node.entry map (composition(path, segment) -> _)
       }
     }
 
     def members = {
       _members.toIterable withFilter {
-        case (segment, node) => node.value isDefined
+        case (segment, node) => node.entry isDefined
       }
     }
   } // Node
 
-  private final class Root[K >: Null <: AnyRef, V](implicit map: PathMap[K, V])
+  private final class Root[K >: Null <: AnyRef, V](implicit map: FileSystem[K, V])
   extends Node[K, V](None, None) {
     override def address = map -> None
   } // Root
@@ -233,7 +235,7 @@ object PathMap {
     override def unapply(path: String) = {
       val i = path.lastIndexOf(separator)
       if (0 <= i) {
-        // Don't share sub-strings with the PathMap!
+        // Don't share sub-strings with the FileSystem!
         Some(Some(path substring (0, i)) -> new String(path substring i))
       } else {
         // There's no point in copying here!
@@ -241,4 +243,4 @@ object PathMap {
       } 
     }
   } // StringComposition
-} // PathMap
+} // FileSystem
