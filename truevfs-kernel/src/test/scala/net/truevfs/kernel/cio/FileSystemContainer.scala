aeb198fdd8c1c2e8986c1cc323b01fb8f0dd1976
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-06-01 14:45:47 +0200

    RCB

--- truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/FileSystemContainer.scala
+++ truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/FileSystemContainer.scala
@@ -5,11 +5,10 @@
 package net.truevfs.kernel.cio
 
 import net.truevfs.kernel.util.FileSystem
-import PathMapContainer._
 
-class FileSystemContainer[E >: Null <: Entry] extends Container[Node[E]] {
+class FileSystemContainer[E >: Null <: Entry] extends Container[E] {
 
-  private[this] val map = FileSystem[Node[E]]('/')
+  private[this] val map = FileSystem[E]('/')
 
   final override def size = map size
 
@@ -18,27 +17,9 @@ class FileSystemContainer[E >: Null <: Entry] extends Container[Node[E]] {
     map valuesIterator
   }
 
-  final override def entry(name: String) = map get(name) orNull
+  final override def entry(name: String) = map get name orNull
 
-  protected def newNode(name: => String, entry: E) =
-    new Node(name, entry)
+  def link(name: String, entry: E) = map link (name, entry)
 
-  def add(name: String, entry: E) = {
-    val composition = map composition
-    val Some((parent, segment)) = composition unapply name
-    val node = newNode(composition(parent, segment), entry)
-    map += name -> node
-    node
-  }
-
-  def remove(name: String) {
-    map -= name
-  }
-}
-
-object PathMapContainer {
-  class Node[E <: Entry](name: => String, entry: E)
-  extends DecoratingEntry(entry) {
-    override def getName = name
-  }
+  def unlink(name: String) = map unlink name
 }
