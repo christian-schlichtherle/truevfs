d710f026afc7678c081b76d5c2f13ae72c190a2e
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-06-06 18:25:05 +0200

    Fixed copyright.

--- truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/FileSystemContainerSpec.scala
+++ truevfs-kernel/src/test/scala/net/truevfs/kernel/cio/FileSystemContainerSpec.scala
@@ -1,8 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
-
 package net.truevfs.kernel.cio
 
 import org.junit.runner.RunWith
