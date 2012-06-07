edf30b5e60035b65e9f2343ece1ae4c22c6bd850
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-06-07 22:30:03 +0200

    DRY

--- truevfs-kernel/src/main/scala/de/schlichtherle/truevfs/kernel/ClutchOutputSocket.scala
+++ truevfs-kernel/src/main/scala/de/schlichtherle/truevfs/kernel/ClutchOutputSocket.scala
@@ -6,7 +6,6 @@ package de.schlichtherle.truevfs.kernel
 
 import javax.annotation.concurrent._
 import net.truevfs.kernel.cio._
-import scala.annotation.tailrec
 
 /** An output socket which obtains its delegate socket lazily and `reset`s
   * it upon any [[java.lang.Throwable]].
@@ -17,40 +16,8 @@ import scala.annotation.tailrec
   */
 @NotThreadSafe
 private abstract class ClutchOutputSocket[E <: Entry]
-extends DelegatingOutputSocket[E] {
-
-  private[this] var _socket: Option[OutputSocket[_ <: E]] = None
-
+extends DelegatingOutputSocket[E] with ClutchSocketLike[E, OutputSocket[_ <: E]] {
   override def localTarget() = apply(_.localTarget())
-
   override def stream() = apply(_.stream())
-
   override def channel() = apply(_.channel())
-
-  private def apply[A](f: OutputSocket[_ <: E] => A) = {
-    try { f(boundSocket) }
-    catch { case ex => reset(); throw ex }
-  }
-
-  protected final def reset() { _socket = None }
-
-  @tailrec
-  protected final override def socket: OutputSocket[_ <: E] = {
-    _socket match {
-      case Some(socket) => socket
-      case None => _socket = Some(lazySocket); socket
-    }
-  }
-
-  /** Returns the output socket for lazy initialization.
-    * 
-    * @return the output socket for lazy initialization.
-    * @throws IOException on any I/O error. 
-    */
-  protected def lazySocket: OutputSocket[_ <: E]
-
-  /** Returns a string representation of this object for debugging and logging
-    * purposes.
-    */
-  override def toString = "%s[socket=%s]" format (getClass.getName, socket)
 }
