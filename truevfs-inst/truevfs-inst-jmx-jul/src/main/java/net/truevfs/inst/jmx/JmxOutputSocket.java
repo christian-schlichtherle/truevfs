19af41cb7b3c421bdea4470ccd209c83e5e4f776
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-06-22 00:13:59 +0200

    Split modules.

---
+++ truevfs-inst/truevfs-inst-jmx-jul/src/main/java/net/truevfs/inst/jmx/JmxOutputSocket.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.truevfs.inst.jmx;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.channels.SeekableByteChannel;
+import javax.annotation.concurrent.Immutable;
+import net.truevfs.inst.core.InstrumentingOutputSocket;
+import net.truevfs.kernel.spec.cio.Entry;
+import net.truevfs.kernel.spec.cio.InputSocket;
+import net.truevfs.kernel.spec.cio.OutputSocket;
+
+/**
+ * @author  Christian Schlichtherle
+ */
+@Immutable
+final class JmxOutputSocket<E extends Entry>
+extends InstrumentingOutputSocket<E> {
+
+    final JmxIoStatistics stats;
+
+    JmxOutputSocket(JmxDirector director, OutputSocket<? extends E> model, JmxIoStatistics stats) {
+        super(director, model);
+        assert null != stats;
+        this.stats = stats;
+    }
+
+    @Override
+    public SeekableByteChannel channel(InputSocket<? extends Entry> peer)
+    throws IOException {
+        return new JmxSeekableChannel(socket().channel(peer), stats);
+    }
+
+    @Override
+    public OutputStream stream(InputSocket<? extends Entry> peer)
+    throws IOException {
+        return new JmxOutputStream(socket().stream(peer), stats);
+    }
+}
