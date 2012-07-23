aa0f8a7e23deea1df64e21259865714eaa42221d
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-07-23 08:42:06 +0200

    Cleaned up.

--- truevfs-keymgr/truevfs-keymgr-swing/src/main/java/net/truevfs/keymgr/swing/feedback/Feedback.java
+++ truevfs-keymgr/truevfs-keymgr-swing/src/main/java/net/truevfs/keymgr/swing/feedback/Feedback.java
@@ -7,7 +7,7 @@ package net.truevfs.keymgr.swing.feedback;
 import javax.swing.JPanel;
 
 /**
- * Provides visual and/or audible run to the user when prompting
+ * Provides visual and/or audible feedback to the user when prompting
  * for a key in its {@link #run} method.
  * <p>
  * Note that the {@link #run} method of this class is called when the
@@ -15,12 +15,12 @@ import javax.swing.JPanel;
  * the implementation of this interface is not expected to do anything in
  * particular.
  *
- * @author  Christian Schlichtherle
+ * @author Christian Schlichtherle
  */
 public interface Feedback {
 
     /**
-     * Starts the visual/audible run.
+     * Starts the visual/audible feedback.
      * This method is called when the panel is shown in its containing window.
      * It is run on AWT's Event Dispatch Thread, so it must complete fast
      * in order not to block the GUI.
@@ -28,7 +28,7 @@ public interface Feedback {
      * {@link javax.swing.Timer} class should be used to schedule timer events
      * for the animation.
      *
-     * @param panel the panel to provide visual/audible run to.
+     * @param panel the panel to provide visual/audible feedback to.
      */
     void run(JPanel panel);
 }
