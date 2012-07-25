3a2ec7b08477a32dbeb30e898aa60c1ade0feb99
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-07-25 08:50:22 +0200

    Added NUM_CPU_THREADS.

--- truevfs-kernel/truevfs-kernel-spec/src/test/java/net/truevfs/kernel/spec/util/ConcurrencyUtils.java
+++ truevfs-kernel/truevfs-kernel-spec/src/test/java/net/truevfs/kernel/spec/util/ConcurrencyUtils.java
@@ -13,9 +13,19 @@ import java.util.concurrent.*;
  */
 public class ConcurrencyUtils {
 
-    /** The number of threads for doing I/O considering 90% blocking factor. */
+    /**
+     * The number of threads for doing CPU intensive tasks considering 0%
+     * blocking factor.
+     */
     public static final int
-            NUM_IO_THREADS = 10 * Runtime.getRuntime().availableProcessors();
+            NUM_CPU_THREADS = Runtime.getRuntime().availableProcessors();
+
+    /**
+     * The number of threads for doing I/O intensive tasks considering 90%
+     * blocking factor.
+     */
+    public static final int
+            NUM_IO_THREADS = 10 * NUM_CPU_THREADS;
 
     private ConcurrencyUtils() { }
 
