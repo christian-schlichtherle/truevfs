83965db3f66484e4b4ef1778bb22defbf91150b3
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-08-28 15:21:11 +0200

    Added integration tests for #TRUEZIP-286 .

--- truezip-driver/truezip-driver-tar/src/test/java/de/schlichtherle/truezip/nio/file/tar/TarPathITSuite.java
+++ truezip-driver/truezip-driver-tar/src/test/java/de/schlichtherle/truezip/nio/file/tar/TarPathITSuite.java
@@ -5,7 +5,10 @@
 package de.schlichtherle.truezip.nio.file.tar;
 
 import de.schlichtherle.truezip.fs.FsArchiveDriver;
+import de.schlichtherle.truezip.nio.file.TPath;
 import de.schlichtherle.truezip.nio.file.TPathITSuite;
+import java.io.IOException;
+import org.junit.Test;
 
 /**
  * @param   <D> The type of the archive driver.
@@ -13,6 +16,23 @@ import de.schlichtherle.truezip.nio.file.TPathITSuite;
  */
 public abstract class TarPathITSuite<D extends FsArchiveDriver<?>>
 extends TPathITSuite<D> {
+
+    private static final int LONG_PATH_NAME_LENGTH = 67;
+
+    private static String longPathName() {
+        final StringBuilder sb = new StringBuilder(LONG_PATH_NAME_LENGTH);
+        for (int i = 0; i < LONG_PATH_NAME_LENGTH; i++) sb.append(i % 10);
+        return sb.toString();
+    }
+
+    /** Test for http://java.net/jira/browse/TRUEZIP-286 . */
+    @Test
+    public void testLongPathName() throws IOException {
+        final TPath entry = new TPath(getArchive()).resolve(longPathName());
+        createTestFile(entry);
+        umount();
+        verifyTestFile(entry);
+    }
 
     /**
      * Skipped because appending to TAR files is currently not supported.
