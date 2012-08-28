cf5e4bdf2c03fd8787fbf338b51f0346c208ee8e
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-08-28 15:21:10 +0200

    Added integration tests for #TRUEZIP-286 .

--- truevfs-comp/truevfs-comp-tardriver/src/test/java/net/java/truevfs/comp/tardriver/it/TarPathITSuite.java
+++ truevfs-comp/truevfs-comp-tardriver/src/test/java/net/java/truevfs/comp/tardriver/it/TarPathITSuite.java
@@ -4,8 +4,11 @@
  */
 package net.java.truevfs.comp.tardriver.it;
 
+import java.io.IOException;
+import net.java.truevfs.access.TPath;
 import net.java.truevfs.access.it.TPathITSuite;
 import net.java.truevfs.kernel.spec.FsArchiveDriver;
+import org.junit.Test;
 
 /**
  * @param   <D> The type of the archive driver.
@@ -14,11 +17,30 @@ import net.java.truevfs.kernel.spec.FsArchiveDriver;
 public abstract class TarPathITSuite<D extends FsArchiveDriver<?>>
 extends TPathITSuite<D> {
 
+    private static final int LONG_PATH_NAME_LENGTH = 67;
+
+    private static String longPathName() {
+        final StringBuilder sb = new StringBuilder(LONG_PATH_NAME_LENGTH);
+        for (int i = 0; i < LONG_PATH_NAME_LENGTH; i++) sb.append(i % 10);
+        return sb.toString();
+    }
+
+    /** Test for http://java.net/jira/browse/TRUEZIP-286 . */
+    @Test
+    public void testLongPathName() throws IOException {
+        final TPath entry = new TPath(getArchive()).resolve(longPathName());
+        createTestFile(entry);
+        umount();
+        verifyTestFile(entry);
+    }
+
     /**
-     * Skipped because appending to TAR files is not supported.
+     * Skipped because appending to TAR files is currently not supported.
+     * 
+     * @deprecated 
      */
+    @Deprecated
     @Override
-    @SuppressWarnings("NoopMethodInAbstractClass")
     public final void testGrowing() {
     }
 }
