a37aea65b72582df32d4b4ad503790ca4e4c9bd8
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-09-09 11:37:17 +0200

    Fixed access modifiers.

--- truevfs-kernel/truevfs-kernel-impl/src/main/scala/net/java/truevfs/kernel/impl/DriverModel.scala
+++ truevfs-kernel/truevfs-kernel-impl/src/main/scala/net/java/truevfs/kernel/impl/DriverModel.scala
@@ -9,7 +9,7 @@ import net.java.truevfs.kernel.spec._
 /**
   * @author Christian Schlichtherle
   */
-trait DriverModel[E <: FsArchiveEntry] extends Model {
+private trait DriverModel[E <: FsArchiveEntry] extends Model {
   def driver: FsArchiveDriver[E]
   def touch(options: AccessOptions)
 }
