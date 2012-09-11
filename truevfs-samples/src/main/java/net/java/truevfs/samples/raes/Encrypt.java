dd3b1caf143630c1af1f97861b4f19839631b3e3
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-09-11 14:32:32 +0200

    Done. Issue #TRUEVFS-55 - Refactor Nzip class to enum

--- truevfs-samples/src/main/java/net/java/truevfs/samples/raes/Encrypt.java
+++ truevfs-samples/src/main/java/net/java/truevfs/samples/raes/Encrypt.java
@@ -4,20 +4,16 @@
  */
 package net.java.truevfs.samples.raes;
 
-import net.java.truevfs.kernel.spec.FsSyncException;
-import net.java.truevfs.access.TPath;
-import net.java.truevfs.samples.access.Application;
-import net.java.truevfs.samples.access.IllegalUsageException;
 import java.io.IOException;
-import java.util.ResourceBundle;
+import net.java.truevfs.access.TPath;
 
 /**
  * Encrypts the contents of the file provided as the first argument
  * into the RAES file provided as the second argument for the main method.
  * <p>
  * Please note that you should not use this utility to encrypt a plain
- * ZIP file to an RAES encrypted ZIP file (usually a files with a
- * {@code ".tzp"} or {@code ".zip.rae"} extension).
+ * ZIP file to an RAES encrypted ZIP file (usually a file with a
+ * {@code ".zip.rae"} or {@code ".zip.raes"} extension).
  * This is because RAES encrypted ZIP files use the &quot;UTF-8&quot;
  * as their character set, whereas plain ZIP files use &quot;IBM437&quot;,
  * a.k.a. &quot;CP437&quot;.
@@ -26,22 +22,14 @@ import java.util.ResourceBundle;
  */
 public class Encrypt extends Application {
 
-    private static final String CLASS_NAME = Encrypt.class.getName();
-    private static final ResourceBundle resources
-            = ResourceBundle.getBundle(CLASS_NAME);
-
-    /** Equivalent to {@code System.exit(new Encrypt().run(args));}. */
-    public static void main(final String[] args) throws FsSyncException {
+    public static void main(String[] args) throws IOException {
         System.exit(new Encrypt().run(args));
     }
 
     @Override
-    protected int runChecked(final String[] args)
-    throws IllegalUsageException, IOException {
-        if (args.length != 2)
-            throw new IllegalUsageException(resources.getString("usage"));
+    void runChecked(final String[] args) throws IOException {
+        if (2 != args.length) throw new IllegalArgumentException();
         Raes.encrypt(   new TPath(args[0]).toNonArchivePath(),
                         new TPath(args[1]).toNonArchivePath());
-        return 0;
     }
 }
