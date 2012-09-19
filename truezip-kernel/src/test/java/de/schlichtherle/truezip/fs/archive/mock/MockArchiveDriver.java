a0ac173f398cc129b8e6803cfbabb45f348c6231
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-09-19 20:05:02 +0200

    RCB

--- truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/mock/MockArchiveDriver.java
+++ truezip-kernel/src/test/java/de/schlichtherle/truezip/fs/archive/mock/MockArchiveDriver.java
@@ -6,10 +6,10 @@ package de.schlichtherle.truezip.fs.archive.mock;
 
 import de.schlichtherle.truezip.entry.Entry;
 import de.schlichtherle.truezip.entry.Entry.Type;
+import de.schlichtherle.truezip.fs.FsCharsetArchiveDriver;
 import de.schlichtherle.truezip.fs.FsModel;
 import de.schlichtherle.truezip.fs.FsMountPoint;
 import de.schlichtherle.truezip.fs.FsOutputOption;
-import de.schlichtherle.truezip.fs.FsCharsetArchiveDriver;
 import de.schlichtherle.truezip.socket.*;
 import de.schlichtherle.truezip.test.TestConfig;
 import de.schlichtherle.truezip.util.BitField;
