36c233f17c7585f95d33f869038bca05e509ba4a
christian <christian@schlichtherle.de>
2012-09-20 04:16:06 +0200

    Done. Issue #TRUEZIP-291 - Too restrictive POSIX permissions when creating an archive file on JSE 7

--- truezip-driver/truezip-driver-file/src/main/java/de/schlichtherle/truezip/fs/nio/file/TempFilePool.java
+++ truezip-driver/truezip-driver-file/src/main/java/de/schlichtherle/truezip/fs/nio/file/TempFilePool.java
@@ -6,10 +6,17 @@ package de.schlichtherle.truezip.fs.nio.file;
 
 import de.schlichtherle.truezip.socket.IOPool;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import static java.nio.file.Files.createTempFile;
 import static java.nio.file.Files.deleteIfExists;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import static java.nio.file.attribute.PosixFilePermission.*;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.EnumSet;
+import java.util.Set;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
@@ -27,12 +34,30 @@ final class TempFilePool implements IOPool<FileEntry> {
     private static final Path TEMP_DIR
             = Paths.get(System.getProperty("java.io.tmpdir"));
 
+    private static final boolean IS_POSIX = FileSystems
+            .getDefault()
+            .supportedFileAttributeViews()
+            .contains("posix");
+    private static final FileAttribute<Set<PosixFilePermission>>
+            POSIX_PERMISSIONS = PosixFilePermissions.asFileAttribute(
+                EnumSet.of( OWNER_READ, OWNER_WRITE,
+                            GROUP_READ, GROUP_WRITE,
+                            OTHERS_READ, OTHERS_WRITE));
+    private static final FileAttribute<?>[]
+            POSIX_ATTRIBUTES = new FileAttribute<?>[] { POSIX_PERMISSIONS };
+    private static final FileAttribute<?>[]
+            NO_ATTRIBUTES = new FileAttribute<?>[0];
+
     /**
      * A default instance of this pool.
      * Use this if you don't have special requirements regarding the temp file
      * prefix, suffix or directory.
      */
     static final TempFilePool INSTANCE = new TempFilePool(null, null);
+
+    private static FileAttribute<?>[] attributes() {
+        return IS_POSIX ? POSIX_ATTRIBUTES.clone() : NO_ATTRIBUTES;
+    }
 
     private final @Nullable Path dir;
     private final String name;
@@ -48,7 +73,7 @@ final class TempFilePool implements IOPool<FileEntry> {
 
     @Override
     public Buffer allocate() throws IOException {
-        return new Buffer(createTempFile(dir, name, null), this);
+        return new Buffer(createTempFile(dir, name, null, attributes()), this);
     }
 
     @Override
@@ -59,8 +84,7 @@ final class TempFilePool implements IOPool<FileEntry> {
     /** A temp file pool entry. */
     @NotThreadSafe
     private static final class Buffer
-    extends FileEntry
-    implements Entry<FileEntry> {
+    extends FileEntry implements Entry<FileEntry> {
 
         Buffer(Path file, final TempFilePool pool) {
             super(file);
