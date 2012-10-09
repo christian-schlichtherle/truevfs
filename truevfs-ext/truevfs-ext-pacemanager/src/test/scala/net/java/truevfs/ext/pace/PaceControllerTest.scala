afcaa180788ea6cfcc701de4815627203e9c9b9e
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-09 17:38:44 +0200

    Improved unit test.

--- truevfs-ext/truevfs-ext-pacemanager/src/test/scala/net/java/truevfs/ext/pace/PaceControllerTest.scala
+++ truevfs-ext/truevfs-ext-pacemanager/src/test/scala/net/java/truevfs/ext/pace/PaceControllerTest.scala
@@ -4,6 +4,7 @@
  */
 package net.java.truevfs.ext.pace
 
+import net.java.truecommons.shed.ControlFlowException
 import net.java.truevfs.kernel.spec._
 import net.java.truevfs.kernel.spec.cio._
 import org.junit.runner._
@@ -19,25 +20,41 @@ import org.scalatest.mock._
 @RunWith(classOf[JUnitRunner])
 class PaceControllerTest extends WordSpec with ShouldMatchers with MockitoSugar with OneInstancePerTest {
 
-  "A PaceController" should {
+  "A PaceController" when {
     val manager = mock[PaceManager]
     val back = mock[FsController]
     when(back.getModel) thenReturn mock[FsModel]
     val front = spy(new PaceController(manager, back))
 
-    "call the PaceManager" when {
-      "apply()ing its aspect" in {
+    "apply()ing its aspect" should {
+      "call only PaceManager.retain(*) if the operation fails" in {
+        intercept[ControlFlowException] {
+          front apply (() => throw new ControlFlowException)
+        }
+        verify(manager) retain back
+        verifyNoMoreInteractions(manager)
+      }
+
+      "call only PaceManager.retain(*) and .accessed(*) if the operation succeeded" in {
         val result = new AnyRef
         front apply (() => result) should be theSameInstanceAs (result)
         verify(manager) retain back
         verify(manager) accessed back
+        verifyNoMoreInteractions(manager)
       }
     }
 
-    "not apply() its aspect to sync(*)" in {
-      front sync null
-      verify(front, never()) apply any()
-      verify(back) sync null
+    "calling sync(*)" should {
+      "not apply() its aspect" in {
+        front sync null
+        verify(front, never()) apply any()
+        ()
+      }
+
+      "forward the call to the decorated controller" in {
+        front sync null
+        verify(back) sync null
+      }
     }
   }
 }
