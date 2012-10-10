51f9880d3542a5da34f84e05dbe895cb2443a9d7
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-10 18:02:45 +0200

    Cleaned up CRC32 handling, thereby reverting changeset 3f26eb710a80.

---
+++ truevfs-comp/truevfs-comp-zip/src/main/java/net/java/truevfs/comp/zip/CheckedInputStream.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truevfs.comp.zip;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.Checksum;
+
+/**
+ * An input stream that also maintains a checksum of the data being read.
+ * The checksum can then be used to verify the integrity of the input data.
+ * <p>
+ * In constrast to its super class, this class accepts a parameter to customize
+ * the skip buffer size.
+ *
+ * @author Christian Schlichtherle
+ */
+class CheckedInputStream extends java.util.zip.CheckedInputStream {
+    private final int skipBufferSize;
+
+    /**
+     * Constructs an input stream using the specified checksum and skip buffer
+     * size.
+     * 
+     * @param in the input stream
+     * @param cksum the checksum
+     * @param skipBufferSize the skip buffer size
+     */
+    CheckedInputStream(
+            final InputStream in,
+            final Checksum cksum,
+            final int skipBufferSize) {
+        super(in, cksum);
+        this.skipBufferSize = skipBufferSize;
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        long total = 0;
+        final byte[] buf = new byte[skipBufferSize];
+        for (long len; (len = n - total) > 0; total += len) {
+            len = read(buf, 0, len < buf.length ? (int) len : buf.length);
+            if (len < 0) break;
+        }
+        return total;
+    }
+}
