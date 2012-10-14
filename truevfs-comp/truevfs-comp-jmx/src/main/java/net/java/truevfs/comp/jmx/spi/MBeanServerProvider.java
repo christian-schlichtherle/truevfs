91ca7e1353d7b2a44966aef1d640b4456aa45fbf
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-14 23:03:01 +0200

    Simplified.

--- truevfs-comp/truevfs-comp-jmx/src/main/java/net/java/truevfs/comp/jmx/spi/MBeanServerProvider.java
+++ truevfs-comp/truevfs-comp-jmx/src/main/java/net/java/truevfs/comp/jmx/spi/MBeanServerProvider.java
@@ -4,13 +4,14 @@
  */
 package net.java.truevfs.comp.jmx.spi;
 
+import java.lang.management.ManagementFactory;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.management.MBeanServer;
 import net.java.truecommons.services.ProviderService;
 import net.java.truevfs.comp.jmx.sl.MBeanServerLocator;
 
 /**
- * An abstract service which provides MBean servers.
+ * A service which provides an MBean server.
  * Provider services are subject to service location by the
  * {@link MBeanServerLocator#SINGLETON}.
  * <p>
@@ -18,10 +19,18 @@ import net.java.truevfs.comp.jmx.sl.MBeanServerLocator;
  * the service with the greatest {@linkplain #getPriority() priority} gets
  * selected.
  * <p>
+ * The implementation in this class simply provides the
+ * {@linkplain ManagementFactory#getPlatformMBeanServer() platform MBean server}.
+ * <p>
  * Implementations should be thread-safe.
  * 
  * @author Christian Schlichtherle
  */
 @ThreadSafe
-public abstract class MBeanServerProvider extends ProviderService<MBeanServer> {
+public class MBeanServerProvider extends ProviderService<MBeanServer> {
+
+    @Override
+    public MBeanServer get() {
+        return ManagementFactory.getPlatformMBeanServer();
+    }
 }
