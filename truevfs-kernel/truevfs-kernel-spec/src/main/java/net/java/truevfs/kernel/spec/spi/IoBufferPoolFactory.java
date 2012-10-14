6394f0598c2288595f45b3f7feae912515107b4e
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-14 21:24:14 +0200

    Updated Javadoc.

--- truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/spi/IoBufferPoolFactory.java
+++ truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/spi/IoBufferPoolFactory.java
@@ -13,6 +13,7 @@ import net.java.truevfs.kernel.spec.sl.IoBufferPoolLocator;
  * An abstract service for creating I/O buffer pools.
  * Factory services are subject to service location by the
  * {@link IoBufferPoolLocator#SINGLETON}.
+ * <p>
  * If multiple factory services are locatable on the class path at run time,
  * the service with the greatest {@linkplain #getPriority() priority} gets
  * selected.
