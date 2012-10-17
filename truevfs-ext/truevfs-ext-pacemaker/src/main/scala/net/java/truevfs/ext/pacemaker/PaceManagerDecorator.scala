0fc9f4745cf59352679da618751659290726c322
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-17 14:05:44 +0200

    RCB

--- truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceManagerDecorator.scala
+++ truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceManagerDecorator.scala
@@ -12,8 +12,9 @@ import net.java.truevfs.kernel.spec.spi._
   */
 @deprecated("This class is reserved for exclusive use by the [[net.java.truevfs.kernel.spec.sl.FsManagerLocator.SINGLETON]]!", "1")
 final class PaceManagerDecorator extends FsManagerDecorator {
+
   def apply(manager: FsManager): FsManager = PaceMediator instrument manager
 
-  /**Returns 100. */
+  /** Returns 100. */
   override def getPriority = 100
 }
