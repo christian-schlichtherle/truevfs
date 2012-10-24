18eb28299fc37136432daff454aa9d91e8bda8ce
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-25 00:14:16 +0200

    Updated documentation.

--- truevfs-comp/truevfs-comp-ibm437/src/main/java/net/java/truevfs/comp/ibm437/package-info.java
+++ truevfs-comp/truevfs-comp-ibm437/src/main/java/net/java/truevfs/comp/ibm437/package-info.java
@@ -3,8 +3,13 @@
  * All rights reserved. Use is subject to license terms.
  */
 /**
- * The archive driver family for ZIP files.
- * 
+ * Provides the IBM437 character set, which was implicitly assumed to be used
+ * for ZIP files before version 6-3-0 of PKWARE's ZIP File Format Specification
+ * defined it explicitly and added UTF-8 as an option.
+ * If the JRE provides its own implementation, this package will not be used
+ * and may be safely removed from the class path.
+ *
+ * @see    <a href="http://www.pkware.com/documents/casestudies/APPNOTE.TXT">ZIP File Format Specification</a>
  * @author Christian Schlichtherle
  */
 @javax.annotation.Nonnull @javax.annotation.ParametersAreNonnullByDefault
