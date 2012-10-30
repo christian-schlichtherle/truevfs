669c876ff46d26b8f11ff04d33b07e91533c4de1
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-30 14:13:49 +0100

    Updated documentation.

--- truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/cio/package-info.java
+++ truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/cio/package-info.java
@@ -3,11 +3,9 @@
  * All rights reserved. Use is subject to license terms.
  */
 /**
- * Provides generic services for reading and writing
- * {@linkplain net.java.truevfs.kernel.spec.cio.Entry entries} in
- * {@linkplain net.java.truevfs.kernel.spec.cio.Container containers},
- * e.g.&#160;file systems, archive files or I/O buffer pools.
- * 
+ * Provides a
+ * {@link net.java.truevfs.kernel.spec.cio.MultiplexingOutputService}.
+ *
  * @author Christian Schlichtherle
  */
 @javax.annotation.Nonnull @javax.annotation.ParametersAreNonnullByDefault
