f1753e26c01108a7dbe02376d15249a52e837952
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-11-11 14:46:41 +0100

    Cleanup.

--- truevfs-key/truevfs-key-spec/src/main/java/net/java/truevfs/key/spec/prompting/SharedKeyProvider.java
+++ truevfs-key/truevfs-key-spec/src/main/java/net/java/truevfs/key/spec/prompting/SharedKeyProvider.java
@@ -148,4 +148,96 @@ extends UniqueObject {
     synchronized boolean isChangeRequested() {
         return null != key && key.isChangeRequested();
     }
+
+    /** Implements the behavior strategy for prompting key providers. */
+    private enum State {
+
+        RESET {
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForWriting(
+                    final PromptingKeyProvider<K> provider)
+            throws UnknownKeyException {
+                provider.promptKeyForWriting();
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForReading(
+                    final PromptingKeyProvider<K> provider,
+                    final boolean invalid)
+            throws UnknownKeyException {
+                provider.promptKeyForReading(invalid);
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void resetCancelledKey(
+                    SharedKeyProvider<K> provider) {
+            }
+        },
+
+        SET {
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForWriting(
+                    final PromptingKeyProvider<K> provider)
+            throws UnknownKeyException {
+                if (provider.isChangeRequested())
+                    provider.resetUnconditionally();
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForReading(
+                    final PromptingKeyProvider<K> provider,
+                    final boolean invalid)
+            throws UnknownKeyException {
+                if (invalid) provider.resetUnconditionally();
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void resetCancelledKey(
+                    SharedKeyProvider<K> provider) {
+            }
+        },
+
+        CANCELLED {
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForWriting(
+                    PromptingKeyProvider<K> provider)
+            throws UnknownKeyException {
+                throw exception(provider);
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void setupKeyForReading(
+                    PromptingKeyProvider<K> provider,
+                    boolean invalid)
+            throws UnknownKeyException {
+                throw exception(provider);
+            }
+
+            <K extends PromptingKey<K>> PersistentUnknownKeyException exception(
+                    PromptingKeyProvider<K> provider) {
+                PersistentUnknownKeyException ex = provider.getException();
+                if (null == ex)
+                    provider.setException(ex = new KeyPromptingCancelledException());
+                return ex;
+            }
+
+            @Override
+            <K extends PromptingKey<K>> void resetCancelledKey(
+                    SharedKeyProvider<K> provider) {
+                provider.resetUnconditionally();
+            }
+        };
+
+        abstract <K extends PromptingKey<K>> void setupKeyForWriting(
+                PromptingKeyProvider<K> provider)
+        throws UnknownKeyException;
+
+        abstract <K extends PromptingKey<K>> void setupKeyForReading(
+                PromptingKeyProvider<K> provider,
+                boolean invalid)
+        throws UnknownKeyException;
+
+        abstract <K extends PromptingKey<K>> void resetCancelledKey(
+                SharedKeyProvider<K> provider);
+    } // State
 }
