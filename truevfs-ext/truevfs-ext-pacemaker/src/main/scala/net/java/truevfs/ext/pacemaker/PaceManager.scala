a7202ec8908ecad240263f2e7ca21a469de945cc
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-11-16 17:19:46 +0100

    Code cleanup.

--- truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceManager.scala
+++ truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceManager.scala
@@ -18,6 +18,123 @@ import net.java.truevfs.kernel.spec.sl._
 import scala.math._
 import PaceManager._
 
+private object PaceManager {
+
+  type AnyControllerFilter = Filter[_ >: FsController]
+  type ControllerSyncVisitor = Visitor[_ >: FsController, FsSyncException]
+
+  val logger = new LocalizedLogger(classOf[PaceManager])
+
+  /**
+    * The key string for the system property which defines the value of the
+    * constant `MAXIMUM_FILE_SYSTEMS_MOUNTED_DEFAULT_VALUE`,
+    * which is equivalent to the expression
+    * `PaceManager.class.getPackage().getName() + ".maximumFileSystemsMounted"`.
+    */
+  val maximumFileSystemsMountedPropertyKey =
+    classOf[PaceManager].getPackage.getName + ".maximumFileSystemsMounted"
+
+  /**
+    * The minimum value for the maximum number of mounted file systems,
+    * which is {@value}.
+    */
+  val maximumFileSystemsMountedMinimumValue = 2
+
+  /**
+    * The default value for the maximum number of mounted file systems.
+    * The value of this constant will be set to
+    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_MINIMUM_VALUE` unless a system
+    * property with the key string
+    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_PROPERTY_KEY`
+    * is set to a value which is greater than
+    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_MINIMUM_VALUE`.
+    *
+    * Mind you that this constant is initialized when this interface is loaded
+    * and cannot accurately reflect the value in a remote JVM instance.
+    */
+  val maximumFileSystemsMountedDefaultValue =
+    max(maximumFileSystemsMountedMinimumValue,
+        Integer getInteger (maximumFileSystemsMountedPropertyKey,
+                            maximumFileSystemsMountedMinimumValue))
+
+  private val initialCapacity =
+    HashMaps initialCapacity (maximumFileSystemsMountedDefaultValue + 1)
+
+  private def locked[V](lock: Lock)(operation: => V) = {
+    lock lock ()
+    try { operation }
+    finally { lock unlock () }
+  }
+
+  private final class MountedControllerMap(evicted: ju.Collection[FsController])
+  extends ju.LinkedHashMap[FsMountPoint, FsController](initialCapacity, 0.75f, true) {
+
+    override def removeEldestEntry(entry: ju.Map.Entry[FsMountPoint, FsController]) =
+      if (size > max) evicted.add(entry.getValue) else false
+
+    @volatile
+    private[this] var _max = maximumFileSystemsMountedDefaultValue
+
+    def max = _max
+    def max_=(max: Int) {
+      require (max >= maximumFileSystemsMountedMinimumValue)
+      _max = max
+    }
+
+    def exists(filter: AnyControllerFilter): Boolean = {
+      val it = values.iterator
+      while (it.hasNext) if (filter accept it.next) return true
+      false
+    }
+  } // MountedControllerMap
+
+  private final class MountedControllerSet
+  (evicted: ju.Collection[FsController])
+  (implicit lock: ReentrantReadWriteLock = new ReentrantReadWriteLock) {
+
+    private[this] val map = new MountedControllerMap(evicted)
+    private[this] val readLock = lock.readLock
+    private[this] val writeLock = lock.writeLock
+
+    def max = map.max
+    def max_=(max: Int) { map.max = max }
+
+    def exists(filter: AnyControllerFilter) =
+      locked(readLock)(map exists filter)
+
+    def add(controller: FsController) {
+      val mp = controller.getModel.getMountPoint
+      locked(writeLock)(map put (mp, controller))
+    }
+
+    def sync(manager: FsManager,
+             filter: AnyControllerFilter,
+             visitor: ControllerSyncVisitor) {
+      manager sync (
+        new Filter[FsController] {
+          override def accept(controller: FsController) = {
+            val accepted = filter accept controller
+            if (accepted)
+              locked(writeLock) { map remove controller.getModel.getMountPoint }
+            accepted
+          }
+        },
+        new Visitor[FsController, FsSyncException] {
+          override def visit(controller: FsController) {
+            try {
+              visitor visit controller
+            } finally {
+              val model = controller.getModel
+              if (model.isMounted)
+                locked(writeLock) { map put (model.getMountPoint, controller) }
+            }
+          }
+        }
+      )
+    }
+  } // MountedControllerSet
+}
+
 /** The pace manager.
   *
   * @author Christian Schlichtherle
@@ -87,121 +204,4 @@ extends JmxManager[PaceMediator](mediator, manager) {
     while (it.hasNext) if (filter accept it.next) it remove ()
     mounted sync (manager, filter, visitor)
   }
-}
-
-private object PaceManager {
-
-  type AnyControllerFilter = Filter[_ >: FsController]
-  type ControllerSyncVisitor = Visitor[_ >: FsController, FsSyncException]
-
-  val logger = new LocalizedLogger(classOf[PaceManager])
-
-  /**
-    * The key string for the system property which defines the value of the
-    * constant `MAXIMUM_FILE_SYSTEMS_MOUNTED_DEFAULT_VALUE`,
-    * which is equivalent to the expression
-    * `PaceManager.class.getPackage().getName() + ".maximumFileSystemsMounted"`.
-    */
-  val maximumFileSystemsMountedPropertyKey =
-    classOf[PaceManager].getPackage.getName + ".maximumFileSystemsMounted"
-
-  /**
-    * The minimum value for the maximum number of mounted file systems,
-    * which is {@value}.
-    */
-  val maximumFileSystemsMountedMinimumValue = 2
-
-  /**
-    * The default value for the maximum number of mounted file systems.
-    * The value of this constant will be set to
-    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_MINIMUM_VALUE` unless a system
-    * property with the key string
-    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_PROPERTY_KEY`
-    * is set to a value which is greater than
-    * `MAXIMUM_FILE_SYSTEMS_MOUNTED_MINIMUM_VALUE`.
-    *
-    * Mind you that this constant is initialized when this interface is loaded
-    * and cannot accurately reflect the value in a remote JVM instance.
-    */
-  val maximumFileSystemsMountedDefaultValue =
-    max(maximumFileSystemsMountedMinimumValue,
-        Integer getInteger (maximumFileSystemsMountedPropertyKey,
-                            maximumFileSystemsMountedMinimumValue))
-
-  private val initialCapacity =
-    HashMaps initialCapacity (maximumFileSystemsMountedDefaultValue + 1)
-
-  private def locked[V](lock: Lock)(operation: => V) = {
-    lock lock ()
-    try { operation }
-    finally { lock unlock () }
-  }
-
-  private final class MountedControllerMap(evicted: ju.Collection[FsController])
-  extends ju.LinkedHashMap[FsMountPoint, FsController](initialCapacity, 0.75f, true) {
-
-    override def removeEldestEntry(entry: ju.Map.Entry[FsMountPoint, FsController]) =
-      if (size > max) evicted.add(entry.getValue) else false
-
-    @volatile
-    private[this] var _max = maximumFileSystemsMountedDefaultValue
-
-    def max = _max
-    def max_=(max: Int) {
-      require (max >= maximumFileSystemsMountedMinimumValue)
-      _max = max
-    }
-
-    def exists(filter: Filter[FsController]): Boolean = {
-      val it = values.iterator
-      while (it.hasNext) if (filter accept it.next) return true
-      false
-    }
-  } // MountedControllerMap
-
-  private final class MountedControllerSet
-  (evicted: ju.Collection[FsController])
-  (implicit lock: ReentrantReadWriteLock = new ReentrantReadWriteLock) {
-
-    private[this] val map = new MountedControllerMap(evicted)
-    private[this] val readLock = lock.readLock
-    private[this] val writeLock = lock.writeLock
-
-    def max = map.max
-    def max_=(max: Int) { map.max = max }
-
-    def exists(filter: Filter[FsController]) =
-      locked(readLock)(map exists filter)
-
-    def add(controller: FsController) {
-      val mp = controller.getModel.getMountPoint
-      locked(writeLock)(map put (mp, controller))
-    }
-
-    def sync(manager: FsManager,
-             filter: AnyControllerFilter,
-             visitor: ControllerSyncVisitor) {
-      manager sync (
-        new Filter[FsController] {
-          override def accept(controller: FsController) = {
-            val accepted = filter accept controller
-            if (accepted)
-              locked(writeLock) { map remove controller.getModel.getMountPoint }
-            accepted
-          }
-        },
-        new Visitor[FsController, FsSyncException] {
-          override def visit(controller: FsController) {
-            try {
-              visitor visit controller
-            } finally {
-              val model = controller.getModel
-              if (model.isMounted)
-                locked(writeLock) { map put (model.getMountPoint, controller) }
-            }
-          }
-        }
-      )
-    }
-  } // MountedControllerSet
 }
