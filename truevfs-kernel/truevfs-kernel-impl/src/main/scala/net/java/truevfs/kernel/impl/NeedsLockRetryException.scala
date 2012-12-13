e0bd262710a4751e4a5ac0b5baa1276f54b0c3c1
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-12-13 15:02:00 +0100

    Fixed documentation.

--- truevfs-kernel/truevfs-kernel-impl/src/main/scala/net/java/truevfs/kernel/impl/NeedsLockRetryException.scala
+++ truevfs-kernel/truevfs-kernel-impl/src/main/scala/net/java/truevfs/kernel/impl/NeedsLockRetryException.scala
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
  */
 package net.java.truevfs.kernel.impl
 
@@ -9,7 +9,7 @@ import net.java.truecommons.shed._
 /** Indicates that all file system locks need to get released before the
   * operation can get retried.
   *
-  * @see    SyncController
+  * @see    LockController
   * @author Christian Schlichtherle
   */
 private final class NeedsLockRetryException private ()
