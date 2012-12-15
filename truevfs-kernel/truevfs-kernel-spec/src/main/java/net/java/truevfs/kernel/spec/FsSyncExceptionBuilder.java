267b2897904e64509946982578bf6934eeb59f9a
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-12-15 14:00:34 +0100

    Fixed potential bug in priority comparison.

--- truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsSyncExceptionBuilder.java
+++ truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsSyncExceptionBuilder.java
@@ -30,7 +30,9 @@ extends PriorityExceptionBuilder<FsSyncException> {
 
         @Override
         public int compare(FsSyncException o1, FsSyncException o2) {
-            return o1.getPriority() - o2.getPriority();
+            final int p1 = o1.getPriority();
+            final int p2 = o2.getPriority();
+            return p1 < p2 ? -1 : p1 == p2 ? 0 : 1;
         }
     }
 }
