1513346b6742689c3dd0bb8e7a7ff3d03bd0f2e3
Christian Schlichtherle <christian@schlichtherle.de>
2013-02-22 18:12:12 +0100

    Updated documentation.

--- truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsCompositeDriver.java
+++ truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsCompositeDriver.java
@@ -25,13 +25,30 @@ extends FsModelFactory<FsManager>, FsControllerFactory<FsManager> {
     /**
      * {@inheritDoc}
      * <p>
+     * The file system model gets created by using a
+     * {@link FsDriver file system driver} which gets looked up by querying the
+     * scheme of the given mount point with the
+     * expression {@code mountPoint.getScheme()}.
+     *
+     * @throws ServiceConfigurationError if no appropriate file system driver
+     *         is known for the scheme of the given mount point.
+     */
+    @Override
+    FsModel newModel(
+            FsManager context,
+            FsMountPoint mountPoint,
+            @CheckForNull FsModel parent);
+
+    /**
+     * {@inheritDoc}
+     * <p>
      * The file system controller gets created by using a
      * {@link FsDriver file system driver} which gets looked up by querying the
      * scheme of the mount point of the given file system model with the
      * expression {@code model.getMountPoint().getScheme()}.
      *
      * @throws ServiceConfigurationError if no appropriate file system driver
-     *         is known for the scheme of the given mount point.
+     *         is known for the scheme of the mount point of the given model.
      */
     @Override
     FsController newController(
