39527b1c4afb12ffd1f42bf9fd4179ac5db57f0d
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2013-02-22 18:12:12 +0100

    Updated documentation.

--- truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsDriver.java
+++ truevfs-kernel/truevfs-kernel-spec/src/main/java/net/java/truevfs/kernel/spec/FsDriver.java
@@ -20,11 +20,20 @@ public abstract class FsDriver
 extends UniqueObject
 implements FsModelFactory<FsManager>, FsControllerFactory<FsManager> {
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The implementation in the class {@link FsDriver} simply forwards
+     * the call to the given file system manager.
+     */
     @Override
     public final FsModel newModel(
             FsManager context,
             FsMountPoint mountPoint,
             FsModel parent) {
+        assert null == parent
+                ? null == mountPoint.getParent()
+                : parent.getMountPoint().equals(mountPoint.getParent());
         return context.newModel(this, mountPoint, parent);
     }
 
