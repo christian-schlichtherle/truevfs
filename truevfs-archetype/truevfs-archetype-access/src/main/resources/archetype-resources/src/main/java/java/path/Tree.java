6c130cb6617ac048cb5cc78794ea0dcfe164b721
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2013-04-20 14:11:49 +0200

    Fixed #TRUEVFS-103.

--- truevfs-archetype/truevfs-archetype-access/src/main/resources/archetype-resources/src/main/java/java/path/Tree.java
+++ truevfs-archetype/truevfs-archetype-access/src/main/resources/archetype-resources/src/main/java/java/path/Tree.java
@@ -26,42 +26,41 @@ import net.java.truevfs.kernel.spec.FsSyncException;
  * on the run time class path and the path name argument is {@code archive.zip}
  * and this file actually exists as a ZIP file, then the tree graph of the
  * directory structure of this ZIP file gets printed.
- * 
+ *
  * @author  Christian Schlichtherle
  */
-public class Tree extends Application<RuntimeException> {
+public class Tree extends Application<IOException> {
 
     private static final String DEFAULT_PREFIX  = "|-- ";
     private static final String LAST_PREFIX     = "`-- ";
     private static final String DEFAULT_PADDING = "|   ";
     private static final String LAST_PADDING    = "    ";
 
-    public static void main(String[] args) throws FsSyncException {
+    public static void main(String[] args) throws IOException {
         System.exit(new Tree().run(args));
     }
 
     @Override
-    protected int work(String[] args) {
-        if (0 >= args.length)
-            args = new String[] { "." };
-        for (String arg : args)
-            graph(new TPath(arg), "", "");
+    protected int work(String[] args) throws IOException {
+        if (0 >= args.length) args = new String[] { "." };
+        for (String arg : args) graph(new TPath(arg), "", "");
         return 0;
     }
 
-    private void graph(Path file, String padding, String prefix) {
+    private void graph(
+            final Path file,
+            final String padding,
+            final String prefix)
+    throws IOException {
         if (!exists(file))
             throw new IllegalArgumentException(file + " (file or directory does not exist)");
         System.out.append(padding).append(prefix).println(file.getFileName());
         if (isDirectory(file)) {
-            Set<Path> entries = new TreeSet<>();
+            final Set<Path> entries = new TreeSet<>();
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(file)) {
-                for (Path member : stream)
-                    entries.add(member);
-            } catch (IOException ex) {
-                throw new AssertionError(ex); // unless someone has changed the entry type in between
+                for (Path member : stream) entries.add(member);
             }
-            int l = entries.size() - 1;
+            final int l = entries.size() - 1;
             if (0 <= l) {
                 String nextPadding = padding;
                 if (0 < prefix.length())
