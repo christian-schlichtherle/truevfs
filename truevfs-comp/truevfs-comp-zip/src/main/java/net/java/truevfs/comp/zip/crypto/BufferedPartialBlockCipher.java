cc1c2b45d4bbf338bf7b2f3ccc605ae9cdeb42d8
Christian Schlichtherle <christian@schlichtherle.de>
2015-01-22 17:56:35 +0100

    RCB

--- truevfs-comp/truevfs-comp-zip/src/main/java/net/java/truevfs/comp/zip/crypto/BufferedPartialBlockCipher.java
+++ truevfs-comp/truevfs-comp-zip/src/main/java/net/java/truevfs/comp/zip/crypto/BufferedPartialBlockCipher.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2005-2015 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
 package net.java.truevfs.comp.zip.crypto;
 
 import org.bouncycastle.crypto.BlockCipher;
