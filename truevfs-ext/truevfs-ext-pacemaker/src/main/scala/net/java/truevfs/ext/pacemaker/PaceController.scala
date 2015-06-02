e019911904760c8cd4904982cbc27eec81569fc2
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2015-06-02 11:37:25 +0200

    Fixes documentation.

--- truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceController.scala
+++ truevfs-ext/truevfs-ext-pacemaker/src/main/scala/net/java/truevfs/ext/pacemaker/PaceController.scala
@@ -11,10 +11,10 @@ import net.java.truecommons.shed.OnTryFinally._
 import net.java.truevfs.kernel.spec._
 
 /**
-  * Calls back the given pace manager before and after each file system
-  * operation in order to sync the least recently accessed file systems which
-  * exceed the maximum number of mounted file systems and then register itself
-  * as the most recently accessed file system.
+  * Calls back the given pace manager after each file system operation in order
+  * to sync the least recently accessed file systems which exceed the maximum
+  * number of mounted file systems and then register itself as the most recently
+  * accessed file system.
   *
   * @author Christian Schlichtherle
   */
