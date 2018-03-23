ad6bd5a24c9b364a662db6b21f1608b729f2ae3d
Christian Schlichtherle <christian@schlichtherle.de>
2018-03-23 13:24:17 +0400

    Upgrades dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,17 +22,17 @@ object Dependencies {
   val Slf4jVersion: String = "1.7.25"
 
   val BcprovJdk15on: ModuleID = "org.bouncycastle" % "bcprov-jdk15on" % "1.58"
-  val CommonsCompress: ModuleID = "org.apache.commons" % "commons-compress" % "1.14"
+  val CommonsCompress: ModuleID = "org.apache.commons" % "commons-compress" % "1.16.1"
   val FindbugsAnnotations: ModuleID = "com.google.code.findbugs" % "annotations" % "3.0.1u2" exclude("com.google.code.findbugs", "jsr305") exclude("net.jcip", "jcip-annotations")
-  val Httpclient: ModuleID = "org.apache.httpcomponents" % "httpclient" % "4.5.3" exclude("commons-logging", "commons-logging")
+  val Httpclient: ModuleID = "org.apache.httpcomponents" % "httpclient" % "4.5.5" exclude("commons-logging", "commons-logging")
   val JclOverSlf4j: ModuleID = "org.slf4j" % "jcl-over-slf4j" % Slf4jVersion
   val Junit: ModuleID = "junit" % "junit" % "4.12"
   val JunitInterface: ModuleID = "com.novocode" % "junit-interface" % "0.11"
-  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.10.0"
+  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.16.0"
   val Scalacheck: ModuleID = "org.scalacheck" %% "scalacheck" % "1.13.5"
   def scalaLibrary(scalaVersion: String): ModuleID = "org.scala-lang" % "scala-library" % scalaVersion
   val ScalaPlus: ModuleID = "global.namespace.scala-plus" %% "scala-plus" % "0.1"
-  val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.4"
+  val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.5"
   val Slf4jSimple: ModuleID = "org.slf4j" % "slf4j-simple" % Slf4jVersion
   val TruecommonsAnnotations: ModuleID = "net.java.truecommons" % "truecommons-annotations" % TruecommonsVersion exclude("com.google.code.findbugs", "jsr305") exclude("net.jcip", "jcip-annotations")
   val TruecommonsCIO: ModuleID = "net.java.truecommons" % "truecommons-cio" % TruecommonsVersion
@@ -46,9 +46,9 @@ object Dependencies {
   val TruecommonsKeySwing: ModuleID = "net.java.truecommons" % "truecommons-key-swing" % TruecommonsVersion
   val TruecommonsServices: ModuleID = "net.java.truecommons" % "truecommons-services" % TruecommonsVersion
   val TruecommonsShed: ModuleID = "net.java.truecommons" % "truecommons-shed" % TruecommonsVersion
-  val Xz: ModuleID = "org.tukaani" % "xz" % "1.6"
+  val Xz: ModuleID = "org.tukaani" % "xz" % "1.8"
 
-  val ScalaVersion_2_10: String = sys.props.getOrElse("SCALA_VERSION_2_10", "2.10.6")
-  val ScalaVersion_2_11: String = sys.props.getOrElse("SCALA_VERSION_2_11", "2.11.11")
-  val ScalaVersion_2_12: String = sys.props.getOrElse("SCALA_VERSION_2_12", "2.12.4")
+  val ScalaVersion_2_10: String = sys.env.getOrElse("SCALA_VERSION_2_10", "2.10.7")
+  val ScalaVersion_2_11: String = sys.env.getOrElse("SCALA_VERSION_2_11", "2.11.12")
+  val ScalaVersion_2_12: String = sys.env.getOrElse("SCALA_VERSION_2_12", "2.12.5")
 }
